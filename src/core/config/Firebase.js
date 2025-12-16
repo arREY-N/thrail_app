f3473f0c4964c9f600a4f933b37557bec29b5461
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-17 03:08:46 +0800

    modified: renamed variable names

--- src/core/config/Firebase.js
+++ src/core/config/Firebase.js
@@ -1,15 +1,15 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.EXPO_FIREBASE_APIKEY,
-    authDomain: process.env.EXPO_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.EXPO_FIREBASE_PROJECTID,
-    storageBucket: process.env.EXPO_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.EXPO_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.EXPO_FIREBASE_APPID,
-    measurementId: process.env.EXPO_FIREBASE_MEASUREMENTID
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENTID
 };
 
 let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
