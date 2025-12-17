29797b8d22ae57f91aeb66399ea37d015f4226c4
Specter1031 <Ejcaraig1031@gmail.com>
2025-12-17 16:27:23 +0800

    modified: colors.js

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,2 +1,12 @@
-const BLACK = rgba(10, 10, 10, 1);
-const WHITE = rgba(227, 227, 227, 0);
+export const Colors = {
+    BLACK:          '#111827',
+    WHITE:          '#FFFFFF',
+    Gray:           '#686868',
+    GRAY_MEDIUM:    '#A2A2A2', 
+    GRAY_LIGHT:     '#D9D9D9',
+
+    PrimaryColor:   '#A2A2A2',
+    SecondaryColor: '#D9D9D9',
+
+    Background:     '#FAFAFA',
+};
