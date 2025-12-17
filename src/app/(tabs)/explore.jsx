4c228830f5eada0ef367e1b296a6f65f9e373d43
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-17 16:18:09 +0800

    modified: view mountain screen

--- src/app/(tabs)/explore.jsx
+++ src/app/(tabs)/explore.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
-import ExploreScreen from '@/src/features/Explore/screens/ExploreScreen'
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
 
 export default function explore(){
-    return <ExploreScreen/>
+    const router = useRouter();
+
+    const onViewMountain = () => {
+        router.push('/(mountain)/mountain')
+    }
+
+    return (
+        <View>
+            <Text>Explore</Text>
+            <Pressable onPress={onViewMountain}>
+                <Text>View Mountain</Text>
+            </Pressable>
+        </View>
+        // <ExploreScreen/>
+    )
 }
