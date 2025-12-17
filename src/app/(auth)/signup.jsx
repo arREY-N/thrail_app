757159096032fe4ca0e851e65d95415b118b73c4
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-17 16:16:50 +0800

    modified: added error handling

--- src/app/(auth)/signup.jsx
+++ src/app/(auth)/signup.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
 import { signUp } from '@/src/core/FirebaseAuthUtil';
 import SignUpScreen from '@/src/features/Auth/screens/SignUpScreen';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
 
-export default function signup(){ 
-    const onSignUp = (email, password) => {
-        signUp(email, password);
+export default function signup(){
+    const router = useRouter();
+    const [error, setError] = useState(null);
+
+    const onSignUp = async (email, password) => {
+        setError(null);
+        try{
+            await signUp(email, password);
+            router.replace('(auth)/preference');
+        } catch (err) {
+            console.error('Sign up error:', err.message)
+            setError(err.message);
+        }
     }
 
     return <SignUpScreen onSignUp={onSignUp}/>
