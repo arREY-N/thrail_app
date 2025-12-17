fd5612d513d71428a81ca7876500f3960df8e783
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-17 16:16:15 +0800

    modified: managed error handling

--- src/app/(auth)/login.jsx
+++ src/app/(auth)/login.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { logIn } from '@/src/core/FirebaseAuthUtil'
+import { forgotPassword, logIn } from '@/src/core/FirebaseAuthUtil';
 import LogInScreen from '@/src/features/Auth/screens/LogInScreen';
+import React, { useState } from 'react';
 
 export default function login(){
-    const onLogIn = (email, password) => {
-        logIn(email, password)
+    const [error, setError] = useState(null);
+
+    const onLogIn = async (email, password) => {
+        setError(null);
+        try{
+            await logIn(email, password)
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    const onForgotPassword = async (email) => {
+        setError(null);
+        try{
+            await forgotPassword(email)
+        }catch(error){
+            setError(error.message)
+        }
     }
 
     return (
-        <LogInScreen onLogIn={onLogIn}/>
+        <LogInScreen onLogIn={onLogIn} error={error}/>
     )
 }
