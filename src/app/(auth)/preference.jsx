6a6d6480a25b82e8313dbf21a2fdf00efcbae874
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-17 16:16:33 +0800

    added: preference navigation route

---
+++ src/app/(auth)/preference.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function preference(){
+    return(
+        <View>
+            <Text>Preference Screen</Text>
+        </View>
+    )
+}
