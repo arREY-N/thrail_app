e6fd312d2a538b9a4117b0b7e399b6df2daa19d0
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-19 00:48:29 +0800

    added: User object shape

---
+++ src/core/models/User.js
@@ -0,0 +1,16 @@
+/**
+ * User object shape
+ * @typedef {Object} User
+ * @property {string} uid
+ * @property {string} email
+ * @property {string} username
+ * @property {string} firstname
+ * @property {string} lastname
+ * @property {number|null} number
+ * @property {string} birthday
+ * @property {string} address
+ * @property {boolean} basicInformation
+ * @property {boolean} onBoardingComplete
+ * @property {import("firebase/firestore").Timestamp} createdAt
+ * @property {import("firebase/firestore").Timestamp} updatedAt
+ */
