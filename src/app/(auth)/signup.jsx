6e8b73123688ca28073194fe9734332a333e9697
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-19 00:50:01 +0800

    modified: added input validation

--- src/app/(auth)/signup.jsx
+++ src/app/(auth)/signup.jsx
@@ -7,16 +7,48 @@ export default function signup(){
     const router = useRouter();
     const [error, setError] = useState(null);
 
-    const onSignUp = async (email, password) => {
+    const onSignUp = async (email, username, password, confirmPassword) => {
         setError(null);
         try{
-            await signUp(email, password);
-            router.replace('(auth)/preference');
+            if(!email || !username || !password || !confirmPassword){
+                throw new Error('Please fill up all the necessary information.')
+            }
+
+            if(password !== confirmPassword){
+                throw new Error('Password does not match')
+            }
+
+            const user = await signUp(email, password, username);
+            
+            if(!user) {
+                throw new Error('Sign up failed.');
+            }
+
+            router.replace('/(auth)/information');
         } catch (err) {
             console.error('Sign up error:', err.message)
             setError(err.message);
         }
     }
 
-    return <SignUpScreen onSignUp={onSignUp}/>
+    const onLogIn = () => {
+        router.replace('/(auth)/login');
+    }
+
+    const onBackPress = () => {
+        router.back();
+    }
+
+    const onGmailSignUp = async () => {
+        setError('Function to be added soon.');
+    }
+    
+    return (
+        <SignUpScreen 
+            onSignUp={onSignUp} 
+            onLogin={onLogIn} 
+            onBackPress={onBackPress}
+            onGmailSignUp={onGmailSignUp}
+            error={error}/>
+    )
 }
