4bd7e1dbfa21611b6438b1cf915768f1a0232b64
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-19 00:45:05 +0800

    added: user information navigation route

---
+++ src/app/(auth)/information.jsx
@@ -0,0 +1,60 @@
+import CustomTextInput from "@/src/components/CustomTextInput";
+import { useAuth } from "@/src/core/context/AuthProvider";
+import { updateUserProfile } from "@/src/core/FirebaseAuthUtil";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+
+export default function information(){
+    const { user } = useAuth(); 
+    const [error, setError] = useState(null);
+    
+    const [number, setNumber] = useState();
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [address, setAddress] = useState('');
+
+    const onContinuePress = async () => {
+        setError(null);
+
+        try {
+            if(!number || !firstname || !lastname || !birthday || !address){
+                throw new Error('Please fill up all the necessary information.')
+            }
+            
+            await updateUserProfile(
+                user.uid,
+                {
+                    firstname,
+                    lastname,
+                    number,
+                    birthday,
+                    address
+                } 
+            );
+        } catch (err) {
+            console.log(err.message);
+            setError(err.message);
+        }
+    }
+
+    return(
+        /**
+         * Used for testing purposes only.
+         * Change into proper component <Information/> for frontend development
+         * Please make sure to include all necessary props to pass
+         */
+        <View>
+            {error && <Text>{error}</Text>}
+            <CustomTextInput label={'First name'} placeholder={'Enter first name'} value={firstname} onChangeText={setFirstname}/>
+            <CustomTextInput label={'Last name'} placeholder={'Enter first name'} value={lastname} onChangeText={setLastname}/>
+            <CustomTextInput label={'Birtday'} placeholder={'Enter birthday'} value={birthday} onChangeText={setBirthday}/>
+            <CustomTextInput label={'Contact Number'} placeholder={'Enter contact number'} value={number} onChangeText={setNumber}/>
+            <CustomTextInput label={'Address'} placeholder={'Enter address'} value={address} onChangeText={setAddress}/>
+        
+            <Pressable onPress={onContinuePress}>
+                <Text>Continue</Text>
+            </Pressable>
+        </View>
+    )
+}
