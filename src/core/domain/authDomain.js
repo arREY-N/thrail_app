be89a5d3391959bab5df592ec0a9874cb71b1b9e
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-20 00:36:04 +0800

    modified: added data validation rules

--- src/core/domain/authDomain.js
+++ src/core/domain/authDomain.js
@@ -1,56 +1,71 @@
-import { signUp, updateUserProfile } from '@/src/core/FirebaseAuthUtil';
+import { signUp } from '@/src/core/FirebaseAuthUtil';
 
 export async function onSignUp(
     email, 
     username, 
-    password, 
-    confirmPassword
+    password,
+    phoneNumber,
+    lastname,
+    firstname,
+    birthday,
+    address
 ) {
     try{
-        if(!email || !username || !password || !confirmPassword){
+        if(!phoneNumber || !firstname || !lastname || !birthday || !address){
             throw new Error('Please fill up all the necessary information.')
         }
 
-        if(password !== confirmPassword){
-            throw new Error('Password does not match')
-        }
-
-        const user = await signUp(email, password, username);
+        const user = await signUp(
+            email, 
+            password, 
+            username, 
+            phoneNumber, 
+            lastname, 
+            firstname, 
+            birthday, 
+            address
+        );
         
         if(!user) {
             throw new Error('Sign up failed.');
         }
+
     } catch (err) {
         console.error('Sign up error: ', err.message)
         throw new Error(err.message);
     }
 }
 
-export async function onUpdateUserProfile(
-    uid,
-    firstname,
-    lastname,
-    number,
-    birthday,
-    address
-){
-    try {
-        if(!number || !firstname || !lastname || !birthday || !address){
-            throw new Error('Please fill up all the necessary information.')
-        }
+export function saveSignUp(
+    email, 
+    password, 
+    username, 
+    confirmPassword,
+    setAccount
+){  
+    if(!email || !password || !username || !confirmPassword) throw new Error('Please fill up all information');
+    
+    if(password.length < 8) throw new Error('Password must be at least 8 characters');
 
-        await updateUserProfile(
-            uid,
-            {
-                firstname,
-                lastname,
-                number,
-                birthday,
-                address
-            } 
-        );
-    } catch (err) {
-        console.error('Error updating user profile: ', err.message);
-        throw new Error(err);
-    }
+    if(!/[A-Z]/.test(password)) throw new Error('Password must have at least 1 uppercase character');
+    
+    if(!/[a-z]/.test(password)) throw new Error('Password must have at least 1 lowercase character');
+
+    if(!/\d/.test(password)) throw new Error('Password must have at least 1 number');
+    
+    if(!/[!@#$%^&*]/.test(password)) throw new Error('Password must have at least 1 special character');
+
+    if(password !== confirmPassword) throw new Error('Passwords does not match.');
+
+    if(username.length < 6) throw new Error('Username must be at least 6 characters');
+
+    setAccount(prev => ({
+        ...prev,
+        email,
+        password,
+        username,
+    }))
 }
+// if(!number || !firstname || !lastname || !birthday || !address){
+//     throw new Error('Please fill up all the necessary information.')
+// }
