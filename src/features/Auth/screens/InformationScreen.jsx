3e528b7c3c6911f7956dbaef831481ddfeafa3a6
Specter1031 <Ejcaraig1031@gmail.com>
2025-12-19 14:43:19 +0800

    modified: created props

---
+++ src/features/Auth/screens/InformationScreen.jsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+const InformationScreen = ({ onContinuePress, onBackPress,  }) => {
+    return (
+        <View></View>
+    )
+}
+
+export default InformationScreen;
