4d654ec1380cc8081646da91a4bb1ee1ff6f964c
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-20 12:17:58 +0800

    modified: added preference management functions

--- src/core/context/AccountProvider.js
+++ src/core/context/AccountProvider.js
@@ -1,5 +1,47 @@
 import { createContext, useContext, useState } from "react";
 
+const preferenceTemplate = {
+    q1: {
+        question: 'Have you hiked before',
+        type: 'binary', 
+        options: ['Yes', 'No'], 
+        answer: null,
+        follow: 'q2'
+    },
+    q2: {
+        question: 'Select location',
+        type: 'multi-select', 
+        options: ['Mt. A', 'Mt. B', 'Mt. C', 'Mt. D', 'Mt. E'], 
+        answer: [],
+    },
+    q3: {
+        question: 'What is your hiking experience level',
+        type: 'select', 
+        options: ['Beginner', 'Regular', 'Experienced'], 
+        answer: null,
+    },
+    q4: {
+        question: 'How long do you prefer your hikes to be?',
+        type: 'multi-select', 
+        options: ['1-3 hours', 'half-day', 'full-day', 'overnight', 'multi-day'], 
+        answer: [],
+    },
+    q5: {
+        question: 'Which provinces in Region IV-A (CALABARZON) would you like to explore?',
+        type: 'multi-select', 
+        options: ['Cavite', 'Laguna', 'Batangas', 'Rizal', 'Quezon'], 
+        answer: [],
+    },
+}
+
+const userPreferences = {
+    'hiked': null,
+    'location': null,
+    'experience': null,
+    'hike_length': null,
+    'province': null,
+}
+
 const AccountContext = createContext(null);
 
 export function useAccount(){
@@ -13,6 +55,8 @@ export function useAccount(){
 }
 
 export function AccountProvider({children}){
+    const [questions, setQuestions] = useState(preferenceTemplate);
+    const [preferences, setPreferences] = useState(userPreferences);
     const [account, setAccount] = useState({
         email: '',
         username: '',
@@ -24,12 +68,42 @@ export function AccountProvider({children}){
         birthday: '',
         address: '', 
     });
-    
-    const setValue = ({property, value}) => {
-        setAccount(prev => ({
-            ...prev,
-            [property]: value
-        }));
+
+    const setAnswer = (question, newAnswer) => {
+        setQuestions(prev => {
+            let saveAnswer = newAnswer;
+
+            if(questions[question].type === 'multi-select'){
+                
+                const answers = prev[question].answer                
+
+                saveAnswer = answers.includes(newAnswer) 
+                                ? answers.filter(a => a !== newAnswer)
+                                : [...answers, newAnswer] 
+            }
+
+            return(
+                {
+                    ...prev,
+                    [question]: {
+                        ...prev[question],
+                        answer: saveAnswer 
+                    }
+                }
+            )
+        });
+    }
+
+    const savePreference = () => {
+        const finalPreferences = {
+            hiked: questions['q1'].answer,
+            location: questions['q2']?.answer,
+            experience: questions['q3'].answer,
+            hike_length: questions['q4'].answer,
+            province: questions['q5'].answer,
+        }
+        setPreferences(prev => ({...prev, ...finalPreferences}));
+        return finalPreferences;
     }
 
     const resetAccount = () => {
@@ -47,10 +121,9 @@ export function AccountProvider({children}){
     }
 
     const value = {
-        account,
-        setValue,
-        setAccount,
-        resetAccount
+        account, setAccount, resetAccount,
+        questions, setAnswer,
+        preferences, savePreference,
     }
 
     return <AccountContext.Provider value={value}>{children}</AccountContext.Provider>
