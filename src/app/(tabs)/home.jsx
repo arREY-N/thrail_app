fab84084c89b30b621d0e41c1df4ee7fab64c495
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-20 15:23:23 +0800

    modified: added notes for home functions

--- src/app/(tabs)/home.jsx
+++ src/app/(tabs)/home.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
-import HomeScreen from '@/src/features/Home/screens/HomeScreen'
+import HomeScreen from '@/src/features/Home/screens/HomeScreen';
+import { useRouter } from 'expo-router';
+import React from 'react';
+
+/**
+ * Weather (preview, access button)
+ * Recommendations (list, view all, download)
+ * Trending (list, view all, download)
+ * Notifications
+ * Booking
+ */
 
 export default function home(){
+    const router = useRouter();
+
+    const onWeatherPress = () => {
+        // go to weather page    
+    }
+
     return <HomeScreen/>
 }
