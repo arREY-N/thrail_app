35983c98b51356acb17856e6674426861b1f6c53
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-21 23:14:52 +0800

    added: notification navigation route

---
+++ src/app/(home)/notification.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function notification(){
+    return(
+        <View>
+            <Text>Notification Page</Text>
+        </View>
+    )
+}
