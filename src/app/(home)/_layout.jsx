35a0dc0e5cd6b87c72d4d125ad02f6860bc8ffb9
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-21 23:14:30 +0800

    modified: added home layout

---
+++ src/app/(home)/_layout.jsx
@@ -0,0 +1,35 @@
+import { Stack } from "expo-router";
+
+export default function HomeLayout(){
+    return(
+        <Stack>
+            <Stack.Screen
+                name='weather'
+                options={{
+                    title: 'Weather'
+                }}
+            />
+            
+            <Stack.Screen
+                name='recommendations'
+                options={{
+                    title: 'Recommendation'
+                }}
+            />
+            
+            <Stack.Screen
+                name='trending'
+                options={{
+                    title: 'Trending'
+                }}
+            />
+            
+            <Stack.Screen
+                name='notification'
+                options={{
+                    title: 'Notification'
+                }}
+            />
+        </Stack>
+    )
+}
