2c55267d0d23cd212fbdaf7064dcbdcdc0ad00b6
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-21 23:13:17 +0800

    modified: added recommendation and weather data provider

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,4 +1,6 @@
 import { AuthProvider, useAuth } from '@/src/core/context/AuthProvider';
+import { RecommendationProvider } from '@/src/core/context/RecommendationProvider';
+import { WeatherProvider } from '@/src/core/context/WeatherProvider';
 import { Redirect, Stack } from 'expo-router';
 
 export default function RootLayout() {
@@ -7,8 +9,12 @@ export default function RootLayout() {
 
     return (
         <AuthProvider>
-            <RootNavLayout/>
-            <Stack screenOptions={{headerShown: false}}/>
+            <RecommendationProvider>
+                <WeatherProvider>
+                    <RootNavLayout/>
+                    <Stack screenOptions={{headerShown: false}}/>
+                </WeatherProvider>
+            </RecommendationProvider>
         </AuthProvider>
     );
 }
