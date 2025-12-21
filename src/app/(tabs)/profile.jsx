05b43f432216753ce4f6066db40a475cc1bbf23e
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-21 23:17:45 +0800

    modified: added function to reset recommendations on sign out

--- src/app/(tabs)/profile.jsx
+++ src/app/(tabs)/profile.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { signOut } from 'firebase/auth'
 import { auth } from '@/src/core/config/Firebase';
+import { useRecommendation } from '@/src/core/context/RecommendationProvider';
 import ProfileScreen from '@/src/features/Profile/screens/ProfileScreen';
+import { signOut } from 'firebase/auth';
+import React from 'react';
 
 export default function profile(){
+    const { resetRecommendations } = useRecommendation();
+
     const onSignOut = () => {
         signOut(auth);
+        resetRecommendations();
     }
 
     return <ProfileScreen onSignOut={onSignOut}/>
