742b762a50ccdad1965da0af44643f7381935949
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-21 23:45:34 +0800

    modified: added condition to check if recommendations are loaded

--- src/app/(home)/recommendations.jsx
+++ src/app/(home)/recommendations.jsx
@@ -3,26 +3,29 @@ import { useAppNavigation } from "@/src/core/hook/useAppNavigation";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function recommendations(){
-    const { recommendedTrails } = useRecommendation();
+    const { recommendedTrails, isLoaded } = useRecommendation();
     const { onMountainPress } = useAppNavigation();
 
     return(
         <View>
             <Text>Recommendations page</Text>
             {
-                recommendedTrails.map((r) => {
-                    return(
-                        <Pressable 
-                            onPress={() => onMountainPress(r.id)} 
-                            key={r.id} 
-                            style={styles.recommended}
-                        >    
-                            <Text>Name: {r.name}</Text>
-                            <Text>Length: {r.length}</Text>
-                            <Text>Location: {r.location}</Text>
-                        </Pressable>
-                    )
-                })
+                isLoaded ?
+                    recommendedTrails.map((r) => {
+                        return(
+                            <Pressable 
+                                onPress={() => onMountainPress(r.id)} 
+                                key={r.id} 
+                                style={styles.recommended}
+                            >    
+                                <Text>Name: {r.name}</Text>
+                                <Text>Length: {r.length}</Text>
+                                <Text>Location: {r.location}</Text>
+                            </Pressable>
+                        )
+                    })
+                    : 
+                    (<Text>Recommendations not yet loaded</Text>)
             }
         </View>
     )
