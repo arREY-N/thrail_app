cf9c865b56c0af87b30a57b07ace51e90122797c
Specter1031 <Ejcaraig1031@gmail.com>
2025-12-26 08:55:49 +0800

    modified: modify Preference

--- src/app/(auth)/preference.jsx
+++ src/app/(auth)/preference.jsx
@@ -1,9 +1,9 @@
 import { useAccount } from "@/src/core/context/AccountProvider";
 import { useAuth } from "@/src/core/context/AuthProvider";
 import { finishOnboarding } from "@/src/core/FirebaseAuthUtil";
+import PreferenceScreen from "@/src/features/Auth/screens/Prefence";
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function preference(){
     const router = useRouter();
@@ -22,47 +22,18 @@ export default function preference(){
     }
 
     return(
-        <View>
-            {error && <Text>{error}</Text>}
-            <Text>Preference Screen (Test Screen Only)</Text>
-            {
-                Object.entries(questions).map(([id, value]) => { // iterates through the preferences object
-                    return( // ito yung UI to display each qustion
-                        // dapat may 'key' lagi pag nag i-iterate ng objects
-                        <View style={styles.prefQuestion} key={id}>
-                            {/**value[question] = looks for the specific question */} 
-                            <Text>{ value['question']} </Text>
-                            {
-                                // value['options'] = looks for the options don sa question
-                                value['options'].map(opt => {
-                                    return (
-                                        // setAnswer( question id, then yung sagot)
-                                        <Pressable onPress={() => setAnswer(id, opt)}>
-                                            <Text>{opt}</Text>
-                                        </Pressable>
-                                    )
-                                })
-                            }
-                            {/** test lang kung na-s-save yung answer */}
-                            <Text>Answer: {value['answer']}</Text>
-                        </View>
-                    )
-                })
-            }
+        <PreferenceScreen
+            questions={questions}
+            setAnswer={setAnswer}
+            onFinish={onFinishedPreference}
+            error={error}
+            />
+            // <Pressable onPress={onFinishedPreference}>
+            //     <Text>Finish Onboarding</Text>
+            // </Pressable>
 
-            <Pressable onPress={onFinishedPreference}>
-                <Text>Finish Onboarding</Text>
-            </Pressable>
-
-            <Pressable onPress={() => router.replace('/(tabs)/home')}>
-                <Text>Skip for now</Text>
-            </Pressable>
-        </View>
-    )
+            // <Pressable onPress={() => router.replace('/(tabs)/home')}>
+            //     <Text>Skip for now</Text>
+            // </Pressable>
+    ) 
 }
-
-const styles = StyleSheet.create({
-    prefQuestion: {
-        marginVertical: 10
-    }
-})
