8045050b6c45d8cbe61ec828b8a5c1ae9815ed7c
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-31 00:27:01 +0800

    modified: added role routing

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -2,11 +2,10 @@ import { AuthProvider, useAuth } from '@/src/core/context/AuthProvider';
 import { RecommendationProvider } from '@/src/core/context/RecommendationProvider';
 import { WeatherProvider } from '@/src/core/context/WeatherProvider';
 import { Redirect, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { useTrailsStore } from '../core/stores/trailsStore';
 
 export default function RootLayout() {
-    console.log("Welcome to Thrail: Find thrill in your trails!");
-    console.log("2025");
-
     return (
         <AuthProvider>
             <RecommendationProvider>
@@ -20,13 +19,27 @@ export default function RootLayout() {
 }
 
 const RootNavLayout = () => {
-    const { user, profile, isLoading } = useAuth();
+    const { user, profile, isLoading, role } = useAuth();
+    const { loadTrails } = useTrailsStore();
+    
+    useEffect(() => {
+        console.log("Welcome to Thrail: Find thrill in your trails!");
+        console.log("2025");
+        loadTrails();
+    }, []);
 
-    if(isLoading) return null;
+    const isWeb = false;
 
     if(!user) return <Redirect href={'/(auth)/landing'}/>
+
+    if(role === 'superadmin') return <Redirect href={'/(superadmin)/home'}/>;
     
-    if(!profile?.onBoardingComplete) return <Redirect href={'/(auth)/preference'}/>
-    
-    return <Redirect href={'/(tabs)/home'}/>
+    if(role === 'admin') return <Redirect href={'/(business)/home'}/>;
+
+    if(role === 'user'){
+        if(profile && !profile.onBoardingComplete) return <Redirect href={'/(auth)/preference'}/>
+        return <Redirect href={'/(tabs)/home'}/>
+    }
+
+    return null;
 }
