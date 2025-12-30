d60e742686965c005e623587905aa3dc6195f8a6
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-12-31 00:26:03 +0800

    modified: added testhome component

--- src/app/(tabs)/home.jsx
+++ src/app/(tabs)/home.jsx
@@ -4,7 +4,6 @@ import { useRecommendation } from '@/src/core/context/RecommendationProvider';
 import { useWeather } from '@/src/core/context/WeatherProvider';
 import { useAppNavigation } from '@/src/core/hook/useAppNavigation';
 import { useTrailsStore } from '@/src/core/stores/trailsStore';
-import HomeScreen from '@/src/features/Home/screens/HomeScreen';
 import { useRouter } from 'expo-router';
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
@@ -14,14 +13,15 @@ export default function home(){
     const { locationTemp } = useWeather();
     const { loadLatestRecommendation, recommendedTrails, isLoaded } = useRecommendation();
     const { trails } = useTrailsStore();
-    const { profile } = useAuth();
+    const { profile, role } = useAuth();
     const { onMountainPress, onDownloadPress } = useAppNavigation();
 
     useEffect(() => {
         if(!profile || !profile.uid) return;
         loadLatestRecommendation(profile.uid, trails);
+        console.log('Role: ', role);
     }, [profile]);
-
+    
     const onWeatherPress = () => {
         console.log('to weather page')
         router.push('/(home)/weather')
@@ -47,9 +47,35 @@ export default function home(){
         router.push('/(book)/booking')
     }
 
-    const BACKEND_TEST = true;
+    return (
+        <TESTHOME 
+            locationTemp={locationTemp} 
+            onWeatherPress={onWeatherPress}
+            onViewAllRecommendationPress={onViewAllRecommendationPress}
+            onNotificationPress={onNotificationPress}
+            onBookingPress={onBookingPress}
+            recommendedTrails={recommendedTrails}
+            onMountainPress={onMountainPress}
+            onDownloadPress={onDownloadPress}
+            onViewAllTrendingPress={onViewAllTrendingPress}
+            isLoaded={isLoaded}/>
+    )
+    // return <HomeScreen/>
+}
 
-    if (BACKEND_TEST && isLoaded) return (
+const TESTHOME = ({
+    locationTemp, 
+    onWeatherPress,
+    onViewAllRecommendationPress,
+    onNotificationPress,
+    onBookingPress,
+    recommendedTrails,
+    onMountainPress,
+    onDownloadPress,
+    onViewAllTrendingPress,
+    isLoaded
+}) => {
+    return (
         <View>
             <Text>Location: {locationTemp.location}</Text>
             <Text>Temperature: {locationTemp.temperature}°C</Text>
@@ -61,6 +87,7 @@ export default function home(){
             <CustomButton title={'Notification'} onPress={onNotificationPress}/>
             <CustomButton title={'Booking'} onPress={onBookingPress}/>
             {
+                isLoaded &&
                 recommendedTrails.map((r) => {
                     return (
                         <View>
@@ -76,6 +103,4 @@ export default function home(){
             <CustomButton title={'View all trending'} onPress={onViewAllTrendingPress}/>
         </View>
     )
-
-    return <HomeScreen/>
 }
