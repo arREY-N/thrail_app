288c5984c81402599df9d229bf69d67de3e05d98
Specter1031 <Ejcaraig1031@gmail.com>
2026-01-05 15:25:30 +0800

    modified: modify  styling

--- src/components/ResponsiveScrollView.jsx
+++ src/components/ResponsiveScrollView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet, useWindowDimensions } from 'react-native';
 
-const ResponsiveScrollView = ({ children, minHeight = 600, ...props }) => {
+const ResponsiveScrollView = ({ children, minHeight = 600, contentContainerStyle, ...props }) => {
     
     const { height } = useWindowDimensions();
     
@@ -12,7 +12,8 @@ const ResponsiveScrollView = ({ children, minHeight = 600, ...props }) => {
             style={styles.container}
             contentContainerStyle={[
                 styles.scrollContent,
-                { minHeight: isShortScreen ? minHeight : '100%' }
+                { minHeight: isShortScreen ? minHeight : '100%' },
+                contentContainerStyle
             ]}
             showsVerticalScrollIndicator={false}
             keyboardShouldPersistTaps="handled"
