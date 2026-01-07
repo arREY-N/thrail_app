89d3ec31e54ae1d0d54fc782fc755dc40d7b72d9
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-07 20:04:16 +0800

    added: test loading screen

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const LoadingScreen = () => {
+    return(
+        <View>
+            <Text>THRAIL LOADING</Text>
+        </View>
+    )
+}
+
+export default LoadingScreen
