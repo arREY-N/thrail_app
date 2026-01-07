9c145605f03f61574614ae47c26ad936c3a454eb
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-07 20:06:36 +0800

    added: admin screen routing logic

---
+++ src/app/(admin)/information.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function information(){
+    return(
+        <View>
+            <Text>Business Information Page</Text>
+        </View>
+    )
+}
