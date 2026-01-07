8eeaa2d507c98ae8c25c913c5e0a7a8608344e1b
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-07 20:03:01 +0800

    modified: clean up of root layout for navigation

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,45 +1,22 @@
-import { AuthProvider, useAuth } from '@/src/core/context/AuthProvider';
-import { RecommendationProvider } from '@/src/core/context/RecommendationProvider';
-import { WeatherProvider } from '@/src/core/context/WeatherProvider';
-import { Redirect, Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { useTrailsStore } from '../core/stores/trailsStore';
+import { useAuthStore } from '../core/stores/authStore';
 
 export default function RootLayout() {
-    return (
-        <AuthProvider>
-            <RecommendationProvider>
-                <WeatherProvider>
-                    <RootNavLayout/>
-                    <Stack screenOptions={{headerShown: false}}/>
-                </WeatherProvider>
-            </RecommendationProvider>
-        </AuthProvider>
-    );
-}
+    const initialize = useAuthStore((state) => state.initialize);
 
-const RootNavLayout = () => {
-    const { user, profile, isLoading, role } = useAuth();
-    const { loadTrails } = useTrailsStore();
-    
     useEffect(() => {
-        console.log("Welcome to Thrail: Find thrill in your trails!");
-        console.log("2025");
-        loadTrails();
+        const unsub = initialize();
+        return () => unsub();
     }, []);
 
-    const isWeb = false;
-
-    if(!user) return <Redirect href={'/(auth)/landing'}/>
-
-    if(role === 'superadmin') return <Redirect href={'/(superadmin)/home'}/>;
-    
-    if(role === 'business') return <Redirect href={'/(business)/home'}/>;
-
-    if(role === 'user'){
-        if(profile && !profile.onBoardingComplete) return <Redirect href={'/(auth)/preference'}/>
-        return <Redirect href={'/(tabs)/home'}/>
-    }
-
-    return null;
+    return(
+        <Stack screenOptions = {{headerShown: false}}>
+            <Stack.Screen name="index"/>
+            <Stack.Screen name="(auth)"/>
+            <Stack.Screen name="(tabs)"/>
+            <Stack.Screen name="(superadmin)"/>
+            <Stack.Screen name="(admin)"/>
+        </Stack>
+    )
 }
