b817df7f05a6ea57bc525a1d5bfcbc43ab2eca00
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-12 23:51:33 +0800

    modified: revised routing logic

--- src/app/(superadmin)/_layout.jsx
+++ src/app/(superadmin)/_layout.jsx
@@ -2,18 +2,25 @@ import { SuperAdminProvider } from '@/src/core/context/SuperAdminProvider';
 import { useAuthStore } from '@/src/core/stores/authStore';
 import { Stack, useRouter } from "expo-router";
 import { useEffect } from 'react';
+import LoadingScreen from '../loading';
+import UnauthorizedScreen from '../unauthorized';
 
 export default function SuperAdminLayout(){
     const router = useRouter();
 
-    const user = useAuthStore((state) => state.user);
-    const isLoading = useAuthStore((state) => state.isLoading);
-
+    const user = useAuthStore(s => s.user);
+    const isLoading = useAuthStore(s => s.isLoading);
+    const role = useAuthStore(s => s.role);
+    
     useEffect(() => {
         if(!user && !isLoading) {
             router.replace('/(auth)/landing');
         } 
     }, [user, isLoading]);
+
+    if(!role) return <LoadingScreen/>
+
+    if(role !== 'superadmin') return <UnauthorizedScreen/>
     
     return(     
         <SuperAdminProvider>
