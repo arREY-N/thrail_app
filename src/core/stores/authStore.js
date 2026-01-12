e16b3f9b4e66a4d4928a856adb253d0f5275192d
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-13 00:02:42 +0800

    modified: added sign up functions

--- src/core/stores/authStore.js
+++ src/core/stores/authStore.js
@@ -1,7 +1,21 @@
 import { auth, db } from '@/src/core/config/Firebase';
+import { checkUserCredentials, signUp, logIn } from '@/src/core/repositories/authRepository';
 import { onIdTokenChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { create } from "zustand";
+import { validateInfo, validateSignUp } from '../domain/authDomain';
+
+const accountTemplate = {
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    firstname: '',
+    lastname: '',
+    birthday: '',
+    address: '',
+}
 
 const init = {
     user: null,
@@ -11,10 +25,13 @@ const init = {
     error: null,
     _unsubscribe: null,
     businessId: null,
+    account: accountTemplate
 }
 
 export const useAuthStore = create((set, get) => ({
     ...init,
+    
+    resetSignUp: () => set({account: accountTemplate}),
 
     reset: () => set(init),
 
@@ -22,6 +39,7 @@ export const useAuthStore = create((set, get) => ({
         set({isLoading: true, error: null});
         
         const unsubscribeAuth = onIdTokenChanged(auth, async (firebaseUser) => {
+            console.log('change token')
             const currentUnsub = get()._unsubscribe;
             if(currentUnsub) {
                 currentUnsub();
@@ -40,8 +58,10 @@ export const useAuthStore = create((set, get) => ({
                     setTimeout(() => firebaseUser.getIdToken(true), 2000);
                     return;
                 }
+                
+                const businessId = idTokenResult.claims.businessId || idTokenResult.claims.owner || null;
 
-                set({ user: firebaseUser, role: userRole});
+                set({ user: firebaseUser, role: userRole, businessId});
                 
                 const ref = doc(db, 'users', firebaseUser.uid);
                 const unsubProfile = onSnapshot(ref, (snap) =>{
@@ -72,17 +92,14 @@ export const useAuthStore = create((set, get) => ({
     logIn: async (email, password) => {
         set({isLoading: true, error: null});
         try{
-            const userCredential = await signInWithEmailAndPassword(
-                auth,
-                email,
-                password
-            );
+            const userCredential = await logIn({email, password});
 
             const token = await userCredential.user.getIdTokenResult(true);
             const role = token.claims.role || 'user';
     
             const businessId = token.claims.businessId || token.claims.owner || null;
-                
+            console.log('Store: ', businessId);
+
             set({
                 user: userCredential.user,
                 role: role,
@@ -124,5 +141,59 @@ export const useAuthStore = create((set, get) => ({
                 isLoading: false
             })
         }
+    },
+
+    signUp: async () => {
+        set({ isLoading: true, error: null });
+        try {
+            const user = await signUp(get().account);
+            console.log('User: ', user);
+            set({ isLoading: false })
+        } catch (err) {
+            set({
+                error: err.message || 'Failed signing up',
+                isLoading: false,
+            })
+        }
+    },
+
+    validateSignUp: async () => {
+        set({ isLoading: true, error: null });  
+        try {
+            validateSignUp(get().account);
+
+            await checkUserCredentials(get().account)
+
+            set({ isLoading: false });
+
+            return true;
+        } catch (err) {
+            set({
+                error: err.message || 'Failed checking user credentials',
+                isLoading: false
+            })
+        }
+    },
+
+    validateInfo: () => {
+        set({ isLoading: true, error: null });
+        try {
+            validateInfo(get().account);
+            set({ isLoading: false });
+            return true;
+        } catch (err) {
+            set({
+                error: err.message || 'Failed checking user information',
+                isLoading: false
+            })
+        }
+    },
+
+    editAccount: (userData) => {
+        set((state) => {
+            return {
+                account: {...state.account, ...userData}
+            }
+        })
     }
 }))
