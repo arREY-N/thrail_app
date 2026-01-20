4687a61651c13f857498bad3627eb5e0173cc349
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-21 06:26:43 +0800

    modifief: added role-based routing logic

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,11 @@
-import { useRootNavigationState, useRouter } from "expo-router";
-import { useEffect } from "react";
-import { useAuthStore } from "../core/stores/authStore";
-import LoadingScreen from "./loading";
 import LandingScreen from '@/src/features/Auth/screens/LandingScreen';
+import { Redirect, useRouter } from "expo-router";
+import { useAuthStore } from "../core/stores/authStore";
 
 export default function index() {
     const router = useRouter();
+    const user = useAuthStore(s => s.user);
+    const role = useAuthStore(s => s.role);
 
     const onLogIn = () => {
         router.push('/(auth)/login');
@@ -23,6 +23,14 @@ export default function index() {
         router.push('/(auth)/terms')
     }
 
+    if(user){
+        if(role === 'user') return <Redirect href={'/(tabs)'}/>
+    
+        if(role === 'admin') return <Redirect href={'/(admin)'}/>
+    
+        if(role === 'superadmin') return <Redirect href={'/(superadmin)'}/>
+    }
+    
     return (
         <LandingScreen 
             onLogInPress={onLogIn} 
