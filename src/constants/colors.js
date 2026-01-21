cf07f2de9100a2ce37300daa98061dc53ff6887e
Specter1031 <Ejcaraig1031@gmail.com>
2026-01-21 14:34:03 +0800

    modified: added color samples

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -19,3 +19,62 @@ export const Colors = {
 
     MODAL_OVERLAY:  'rgba(17, 24, 39, 0.5)',
 };
+
+// Color Samples
+// Sample 1 = {
+//     BLACK:          '#0F2815', 
+//     WHITE:          '#FFFFFF',
+//     Gray:           '#607D66',
+//     GRAY_MEDIUM:    '#A5B6A8', 
+//     GRAY_LIGHT:     '#E8F5E9', 
+
+//     PRIMARY:        '#2E7D32', 
+//     SECONDARY:      '#81C784',
+//     BACKGROUND:     '#FAFCFA', 
+
+//     // Utilities
+//     ERROR:          '#D32F2F',
+//     ERROR_BG:       '#FFEBEE',
+//     ERROR_BORDER:   '#FFCDD2',
+//     SUCCESS:        '#388E3C',
+//     WARNING:        '#FBC02D',
+//     MODAL_OVERLAY:  'rgba(15, 40, 21, 0.5)',
+// }
+
+// Sample 2 = {
+//     BLACK:          '#121212',
+//     WHITE:          '#FFFFFF',
+//     Gray:           '#757575',
+//     GRAY_MEDIUM:    '#BDBDBD', 
+//     GRAY_LIGHT:     '#EEEEEE',
+
+//     PRIMARY:        '#009688',
+//     SECONDARY:      '#263238',
+//     BACKGROUND:     '#F4F4F4',
+
+//     ERROR:          '#D32F2F',
+//     ERROR_BG:       '#FFEBEE',
+//     ERROR_BORDER:   '#FFCDD2',
+//     SUCCESS:        '#00C853',
+//     WARNING:        '#FFAB00',
+//     MODAL_OVERLAY:  'rgba(0, 0, 0, 0.6)',
+// }
+
+// Sample 3 = {
+//     BLACK:          '#121212',
+//     WHITE:          '#FFFFFF',
+//     Gray:           '#757575',
+//     GRAY_MEDIUM:    '#BDBDBD', 
+//     GRAY_LIGHT:     '#EEEEEE',
+
+//     PRIMARY:        '#013F4A',
+//     SECONDARY:      '#068562',
+//     BACKGROUND:     '#FAFAFA',
+
+//     ERROR:          '#D32F2F',
+//     ERROR_BG:       '#FFEBEE',
+//     ERROR_BORDER:   '#FFCDD2',
+//     SUCCESS:        '#00C853',
+//     WARNING:        '#FFAB00',
+//     MODAL_OVERLAY:  'rgba(0, 0, 0, 0.6)',
+// }
