cdd85e3aeb43f0e6b08e05ff7365158a83f22e72
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-22 22:23:34 +0800

    modified: removed unused function

--- src/core/FirebaseAuthUtil.js
+++ src/core/FirebaseAuthUtil.js
@@ -1,6 +1,4 @@
-import { auth, db } from '@/src/core/config/Firebase';
-import { getAuthErrorMessage } from '@/src/core/error/autherror';
-import { sendPasswordResetEmail } from 'firebase/auth';
+import { db } from '@/src/core/config/Firebase';
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 export const finishOnboarding = async (uid, preferences) =>{
@@ -17,17 +15,4 @@ export const finishOnboarding = async (uid, preferences) =>{
         },
         {merge: true},
     );
-}
-
-export const forgotPassword = async (email) => {
-    if(!email){
-        throw new Error('Email is required');
-    }
-
-    try {
-        await sendPasswordResetEmail(auth, email)
-    } catch (err) {
-        console.error('Error sending password reset email: ', err.message);
-        throw new Error(getAuthErrorMessage(err));
-    }
 }
