9e2281f536ccf686685f24312fced185a3889883
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-22 22:23:22 +0800

    modified: clean up

--- src/app/(tabs)/explore.jsx
+++ src/app/(tabs)/explore.jsx
@@ -1,14 +1,14 @@
 import { useTrailsStore } from '@/src/core/stores/trailsStore';
+import ExploreScreen from '@/src/features/Explore/screens/ExploreScreen';
 import { useRouter } from 'expo-router';
 import React from 'react';
 // import { Pressable, Text, View } from 'react-native';
 
-import ExploreScreen from '../../features/Explore/screens/ExploreScreen';
 
 export default function explore(){
     const router = useRouter();
 
-    const { trails } = useTrailsStore();
+    const trails = useTrailsStore(s => s.trails);
 
     const onViewMountain = (id) => {  
         console.log(id);
