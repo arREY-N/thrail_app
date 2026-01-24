c8d1c42cada9e9f9504e394e1ee37c598e013651
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-24 10:55:40 +0800

    modified: added routing back to home after submitting application

--- src/app/(business)/apply.jsx
+++ src/app/(business)/apply.jsx
@@ -1,27 +1,27 @@
 import CustomTextInput from '@/src/components/CustomTextInput';
 import { useApplicationsStore } from '@/src/core/stores/applicationsStore';
 import { useAuthStore } from '@/src/core/stores/authStore';
+import { useRouter } from 'expo-router';
 import { useState } from 'react';
 import { Pressable, ScrollView, Text } from 'react-native';
 
 export default function apply(){
+    const router = useRouter();
+
     const [system, setSystem] = useState(null);
-    const profile = useAuthStore((state) => state.profile);
+    const profile = useAuthStore(s => s.profile);
 
     const provinces = ['Cavite', 'Laguna', 'Batangas', 'Rizal', 'Quezon'];
-    const createApplication = useApplicationsStore((state) => state.createApplication);
-    const error = useApplicationsStore((state) => state.error);
+    const createApplication = useApplicationsStore(s => s.createApplication);
+    const error = useApplicationsStore(s => s.error);
 
     const onApplyPress = async (businessData) => {
-        setSystem(null);
-        
         try{           
             const appId = await createApplication({
                 ...businessData,
                 userId: profile.id, 
             });
-
-            setSystem(`Application ${appId} sent`);            
+            router.replace('/(tabs)');             
         } catch (err) {
             setSystem(error ? error.message : err.message );
         }
