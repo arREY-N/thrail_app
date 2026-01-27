bf0a93a5ab78e2ad08c8a9fc9af7cedcffcfc3ba
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-01-27 11:54:09 +0800

    added: created constants file for offer details

---
+++ src/constants/offers.js
@@ -0,0 +1,19 @@
+export const OFFER_CONSTANTS = {
+    DOCUMENT_LIST: [
+        'Valid ID', 
+        'Medical Certificate', 
+        'Waiver' 
+    ],
+    HIKE_DURATION: [
+        '1-3 hours', 
+        'Half-day', 
+        'Full-day', 
+        'Overnight', 
+        'Multi-day'
+    ],
+    OFFER_INCLUSIONS: [
+        'Shuttle', 
+        'Tent', 
+        'Bag tag'
+    ],
+}
