a4493129a2a7319f935b3b91ba5804962d054ec3
Specter1031 <Ejcaraig1031@gmail.com>
2026-01-27 14:10:10 +0800

    modified: added notif and booking

--- src/core/hook/useAppNavigation.js
+++ src/core/hook/useAppNavigation.js
@@ -1,24 +1,34 @@
-import { useRouter } from "expo-router";
+import { useRouter } from 'expo-router';
 
-export function useAppNavigation(){
+export function useAppNavigation() {
     const router = useRouter();
 
     const onMountainPress = (id) => {
         console.log('Routing to: ', id);
-        router.push(`/(mountain)/${id}`)
-    }
+        router.push(`/(mountain)/${id}`);
+    };
 
     const onBackPress = () => {
         router.back();
-    }
+    };
 
     const onDownloadPress = (id) => {
         console.log('Downloading: ', id);
-    }
+    };
 
-    return{
+    const onNotificationPress = () => {
+        router.push('/(home)/notification');
+    };
+
+    const onBookingPress = () => {
+        router.push('/(book)/userBooking');
+    };
+
+    return {
         onMountainPress,
         onBackPress,
-        onDownloadPress
-    }
+        onDownloadPress,
+        onNotificationPress,
+        onBookingPress,
+    };
 }
