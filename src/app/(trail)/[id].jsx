401832af5de0f4981a122fc50214205c35aa6406
Specter1031 <Ejcaraig1031@gmail.com>
2026-02-06 11:44:35 +0800

    created: create unfinished UI for TrailScreen

--- src/app/(trail)/[id].jsx
+++ src/app/(trail)/[id].jsx
@@ -1,8 +1,10 @@
-import CustomButton from "@/src/components/CustomButton";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import React from 'react';
+import { Text } from "react-native";
+
 import { useAppNavigation } from "@/src/core/hook/useAppNavigation";
 import { useTrailsStore } from "@/src/core/stores/trailsStore";
-import { useLocalSearchParams, useRouter } from "expo-router";
-import { Text, View } from "react-native";
+import TrailScreen from "@/src/features/Trail/screens/TrailScreen";
 
 export default function trail(){
     const { id } = useLocalSearchParams();
@@ -11,6 +13,10 @@ export default function trail(){
     const router = useRouter();
 
     const trail = trails.find((t) => t.id === id ? t : null);
+
+    const onBackPress = () => {
+        router.back();
+    }
 
     const onBookPress = (id) => {
         router.push(`/(offer)/${id}`)
@@ -23,35 +29,42 @@ export default function trail(){
     if(!trail) return <Text>Trail {id} not found</Text>
     
     return(
-        <TESTTRAIL 
+        <TrailScreen 
             trail={trail}
-            onDownloadPress={onDownloadPress}
+            onBackPress={onBackPress}
+            onDownloadPress={() => onDownloadPress(id)}
             onBookPress={onBookPress}
             onHikePress={onHikePress}
         />
-    )    
+        // <TESTTRAIL 
+        //     trail={trail}
+        //     onDownloadPress={onDownloadPress}
+        //     onBookPress={onBookPress}
+        //     onHikePress={onHikePress}
+        // />
+    );
 }
 
-const TESTTRAIL = ({
-    trail,
-    onDownloadPress,
-    onBookPress,
-    onHikePress
-}) => {
-    const general = trail?.general;
+// const TESTTRAIL = ({
+//     trail,
+//     onDownloadPress,
+//     onBookPress,
+//     onHikePress
+// }) => {
+//     const general = trail?.general;
 
-    return(
-        <View>
-            <Text>Trail View</Text>
-            <Text>ID: {trail?.id}</Text>
-            <Text>Name: {general.name}</Text>
-            <Text>Province: {general.province?.join(',')}</Text>
-            <Text>Address: {general.address}</Text>
+//     return(
+//         <View>
+//             <Text>Trail View</Text>
+//             <Text>ID: {trail?.id}</Text>
+//             <Text>Name: {general.name}</Text>
+//             <Text>Province: {general.province?.join(',')}</Text>
+//             <Text>Address: {general.address}</Text>
             
-            <CustomButton title={'Download'} onPress={() => onDownloadPress(trail?.id)}/>
-            <CustomButton title={'Book'} onPress={() => onBookPress(trail?.id)}/>
-            <CustomButton title={'Hike'} onPress={() => onHikePress(trail?.id)}/>
-        </View>
+//             <CustomButton title={'Download'} onPress={() => onDownloadPress(trail?.id)}/>
+//             <CustomButton title={'Book'} onPress={() => onBookPress(trail?.id)}/>
+//             <CustomButton title={'Hike'} onPress={() => onHikePress(trail?.id)}/>
+//         </View>
 
-    )
-}
+//     )
+// }
