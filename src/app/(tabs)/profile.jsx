12b7be6aa430390c26f884a40a451bfa02e682e6
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-02-07 12:16:32 +0800

    modified: added buttons for superadmin and admin dashboard access

--- src/app/(tabs)/profile.jsx
+++ src/app/(tabs)/profile.jsx
@@ -3,26 +3,69 @@ import { useRouter } from 'expo-router';
 import React from 'react';
 
 import ProfileScreen from '@/src/features/Profile/screens/ProfileScreen';
+import { StyleSheet, View } from 'react-native';
+
+import CustomButton from '@/src/components/CustomButton';
 
 export default function profile(){
     const router = useRouter();
+    const role = useAuthStore(s => s.role);
 
     const signOut = useAuthStore(s => s.signOut);
     const profile = useAuthStore(s => s.profile);
 
-    const onSignOutPress = async () => {
+    async function onSignOutPress(){
         await signOut();
     }
 
-    const onApplyPress = () => {
+    function onApplyPress(){
         router.push('/(main)/business/apply');
     }
 
+    function onAdminPress(){
+        console.log('to implement');
+    }
+    
+    function onSuperadminPress(){
+        router.push('/(main)/superadmin');
+    }
+
     return (
-        <ProfileScreen
-            onSignOutPress={onSignOutPress}
-            onApplyPress={onApplyPress}
-            profile={profile}
-        />
+        <View>
+            <ProfileScreen
+                onSignOutPress={onSignOutPress}
+                onApplyPress={onApplyPress}
+                profile={profile}
+            />
+            { role === 'superadmin' &&
+                <View>
+                    <CustomButton 
+                        title="Superadmin Dashboard" 
+                        onPress={onSuperadminPress} 
+                        variant="primary"
+                        style={styles.buttonSpacing}
+                    />
+                </View>  
+            }
+
+            { role === 'admin' &&
+                <View>
+                    <CustomButton 
+                        title="Admin Dashboard" 
+                        onPress={onAdminPress} 
+                        variant="primary"
+                        style={styles.buttonSpacing}
+                    />
+                </View>  
+            }
+
+            <View style={{margin: 50}}/>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonSpacing: {
+        marginBottom: 8,
+    }
+});
