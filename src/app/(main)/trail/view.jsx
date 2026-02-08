194a7029dc6291ab2f08210f825be96047b3420a
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-02-08 21:38:59 +0800

    modified: added temporary edit button on returned UI screen

--- src/app/(main)/trail/view.jsx
+++ src/app/(main)/trail/view.jsx
@@ -3,13 +3,17 @@ import useSuperadmin from "@/src/core/hook/useSuperadmin";
 import useTrailHook from "@/src/core/hook/useTrailHook";
 import TrailScreen from "@/src/features/Trail/screens/TrailScreen";
 import { useLocalSearchParams } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function viewTrail(){
     const { trailId } = useLocalSearchParams();
 
     const { onBackPress, onDownloadPress } = useAppNavigation();
 
-    const { role, onWriteTrail } = useSuperadmin();
+    const { 
+        role, 
+        onWriteTrail 
+    } = useSuperadmin();
 
     const {
         trail,
@@ -18,14 +22,22 @@ export default function viewTrail(){
     } = useTrailHook({ trailId, mode: 'view' });
     
     return(
-        <TrailScreen 
-            role={role}
-            trail={trail} 
-            onBackPress={onBackPress} 
-            onDownloadPress={onDownloadPress} 
-            onHikePress={onHikePress}
-            onBookPress={onBookPress}
-            onEditPress={onWriteTrail}
-        />
+        <>
+            { role === 'superadmin' && 
+                <Pressable onPress={() => onWriteTrail(trailId)}>
+                    <Text>EDIT</Text>
+                </Pressable>
+            }
+
+            <TrailScreen 
+                role={role}
+                trail={trail} 
+                onBackPress={onBackPress} 
+                onDownloadPress={onDownloadPress} 
+                onHikePress={onHikePress}
+                onBookPress={onBookPress}
+                onEditPress={onWriteTrail}
+            />
+        </>
     )
 }
