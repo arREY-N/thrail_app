b087ad978d64d6f9ce8f9f70846a5c337158c708
Specter1031 <Ejcaraig1031@gmail.com>
2026-02-09 04:22:18 +0800

    refactor: modify AuthLayout structure and styling for improved navigation

--- src/app/(auth)/_layout.jsx
+++ src/app/(auth)/_layout.jsx
@@ -1,10 +1,29 @@
 import { AccountProvider } from '@/src/core/context/AccountProvider';
 import { Stack } from "expo-router";
 
+import { Colors } from '@/src/constants/colors';
+
 export default function AuthLayout(){
     return(     
         <AccountProvider>
-            <Stack screenOptions={{headerShown: false}}/>
+            <Stack 
+                screenOptions={{
+                    headerShown: false,
+                    contentStyle: { backgroundColor: Colors.BACKGROUND } 
+                }}
+            >
+                <Stack.Screen 
+                    name="tac" 
+                    options={{
+                        presentation: 'transparentModal',
+                        animation: 'fade',
+                        contentStyle: { backgroundColor: 'transparent' } 
+                    }}
+                />
+                <Stack.Screen name="signup" />
+                <Stack.Screen name="information" />
+                <Stack.Screen name="login" />
+            </Stack>
         </AccountProvider>
     )
 }
