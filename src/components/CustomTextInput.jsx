cf12362263996bbfabca019ee5a9fd032e268b39
Specter1031 <Ejcaraig1031@gmail.com>
2026-02-23 18:19:16 +0800

    delete

--- src/components/CustomTextInput.jsx
+++ src/components/CustomTextInput.jsx
@@ -13,23 +13,7 @@ import CustomDateInput from '@/src/components/CustomDateInput';
 import CustomIcon from '@/src/components/CustomIcon';
 import CustomText from '@/src/components/CustomText';
 
-type TextInputParams = {
-    label: string | null, 
-    placeholder, 
-    value, 
-    onChangeText, 
-    secureTextEntry: boolean | false, 
-    keyboardType,
-    isPasswordVisible, 
-    onTogglePassword,
-    type = 'text',
-    style,
-    icon, 
-    iconLibrary = 'Feather', 
-    prefix, 
-    children,
-    ...props
-}
+
 
 const CustomTextInput = ({ 
     label, 
