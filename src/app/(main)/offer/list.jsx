6c03510f5073b3f9f8d478cd736278c1d128f7e6
Specter1031 <Ejcaraig1031@gmail.com>
2026-02-28 11:04:10 +0800

    modified: added new dummy data in offers

--- src/app/(main)/offer/list.jsx
+++ src/app/(main)/offer/list.jsx
@@ -40,25 +40,54 @@ export default function listOffer(){
 const DUMMY_OFFERS = [
     {
         id: 'offer-1',
-        price: 500,
-        date: 'Nov 30, 2025',
-        duration: '1 Day',
-        documents: ['Medical Certificate', 'Valid ID'],
-        inclusions: ['Certified Local Guide', 'First-aid Support'],
-        description: 'A great beginner-friendly hike.',
-        trail: { id: 'trail-1', name: 'Mt. Parawagan' },
-        business: { name: 'Guide/Business Name 1' }
+        price: 1999,
+        date: 'Mar 14, 2026', 
+        duration: '2 Days',
+        business: { name: 'Basic Mountaineering Course' },
+        description: 'Learn the basics of mountaineering while enjoying the scenic views of Mt. Tagapo. Topics: Leave No Trace, Tent Pitching, Camp Management.',
+        inclusions: ['Roundtrip van & boat', 'Guide & Camp fee', 'Registration fee', 'BMC digital certificate'],
+        documents: ['Valid ID'],
+        schedule: [
+            {
+                day: 'Day 1',
+                activities: [
+                    { time: '3:00 AM', event: 'Meet at Mayflower Parking' },
+                    { time: '5:30 AM', event: 'Buy supplies / Start lecture' },
+                    { time: '9:00 AM', event: 'Arrival at office / Start hike' },
+                    { time: '1:30 PM', event: 'Arrival at the summit' },
+                    { time: '6:00 PM', event: 'Food preparation / Socials' },
+                ]
+            },
+            {
+                day: 'Day 2',
+                activities: [
+                    { time: '7:00 AM', event: 'Wake-up call' },
+                    { time: '9:00 AM', event: 'Start descent' },
+                    { time: '11:00 AM', event: 'Wash up' },
+                    { time: '4:00 PM', event: 'Arrival in Manila' },
+                ]
+            }
+        ],
+        thingsToBring: [
+            'Water bottle', 'Snacks & Trail food', 'Extra Clothes', 
+            'Hygiene kit', 'First-aid kit', 'Tent', 'Sleeping gear'
+        ],
+        reminders: [
+            'PHP 1,000 reservation fee is required to secure your slot.',
+            'The reservation fee is NON-REFUNDABLE but transferable.',
+            'Balance must be paid 10 days before the event.',
+            'Organizer may cancel if 12 participants are not met.'
+        ]
     },
     {
         id: 'offer-2',
         price: 400,
-        date: 'Nov 30, 2025',
+        date: 'Mar 14, 2026',
         duration: '1 Day',
         documents: ['Valid ID'],
         inclusions: ['Local Guide'],
-        description: 'Standard hike package.',
-        trail: { id: 'trail-1', name: 'Mt. Parawagan' },
-        business: { name: 'Guide/Business Name 2' }
+        description: 'Standard hike package. Good for beginners.',
+        business: { name: 'Local Guide Coop' }
     },
     {
         id: 'offer-3',
