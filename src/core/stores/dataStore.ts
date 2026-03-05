314e96c9b723c2cfaa1ef7104f2b646126826608
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-05 08:10:16 +0800

    modified: changed to ts

--- src/core/stores/dataStore.ts
+++ src/core/stores/dataStore.ts
@@ -1,18 +1,17 @@
 import { useApplicationsStore } from '@/src/core/stores/applicationsStore';
+import { useBookingsStore } from '@/src/core/stores/bookingsStore';
 import { useBusinessesStore } from '@/src/core/stores/businessesStore';
 import { useOffersStore } from '@/src/core/stores/offersStore';
+import { usePaymentsStore } from '@/src/core/stores/paymentsStore';
 import { useRecommendationsStore } from '@/src/core/stores/recommendationsStore';
 import { useTrailsStore } from '@/src/core/stores/trailsStore';
 import { useUsersStore } from '@/src/core/stores/usersStore';
 import { useWeatherStore } from '@/src/core/stores/weatherStore';
-import { useBookingsStore } from './bookingsStore';
-import { usePaymentsStore } from './paymentsStore';
 
 
 export const resetData = () => {
     try {
         useApplicationsStore.getState().reset();
-        // useAuthStore.getState().reset();
         useBookingsStore.getState().reset();
         useBusinessesStore.getState().reset();
         useOffersStore.getState().reset();
@@ -22,7 +21,7 @@ export const resetData = () => {
         useUsersStore.getState().reset();
         useWeatherStore.getState().reset();
     } catch (err) {
-        throw new Error(err.message || 'Failed resetting data');
+        throw new Error((err as Error).message || 'Failed resetting data');
     }
 }
 
