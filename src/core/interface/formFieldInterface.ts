d6609341ce38acffd998baa0270e08c297b05e60
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-06 10:11:35 +0800

    modified: added date as a type option

--- src/core/interface/formFieldInterface.ts
+++ src/core/interface/formFieldInterface.ts
@@ -2,7 +2,7 @@ export interface IFormField<T> {
     section: T;
     id: string;
     label: string;
-    type: 'text' | 'numerical' | 'boolean' | 'single-select' | 'multi-select' | 'object-select' | 'file';
+    type: 'text' | 'numerical' | 'boolean' | 'single-select' | 'multi-select' | 'object-select' | 'file' | 'date';
     required: boolean;
     options?: string;
     key?: string;
