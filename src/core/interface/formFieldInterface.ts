2777b41efadfc0711460047618b2516a6d7145ab
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-06 09:02:10 +0800

    modified: section data type to make it dynamic

--- src/core/interface/formFieldInterface.ts
+++ src/core/interface/formFieldInterface.ts
@@ -1,5 +1,5 @@
-export interface IFormField {
-    section: 'general' | 'geography' | 'difficulty' | 'tourism' | 'root';
+export interface IFormField<T> {
+    section: T;
     id: string;
     label: string;
     type: 'text' | 'numerical' | 'boolean' | 'single-select' | 'multi-select' | 'object-select' | 'file';
