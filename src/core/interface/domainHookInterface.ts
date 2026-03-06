6d36d7fe6a2a221ff28ce43fb323528f0570d0a7
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-06 09:01:46 +0800

    modified: defined interfaces for write hooks and added documentation comments

--- src/core/interface/domainHookInterface.ts
+++ src/core/interface/domainHookInterface.ts
@@ -1,3 +1,5 @@
+import { IFormField } from "@/src/core/interface/formFieldInterface";
+
 export interface IBaseDomainHook{
     /** Indicates if a store is currently loading or processing */
     isLoading: boolean;
@@ -7,14 +9,54 @@ export interface IBaseDomainHook{
 }
 
 export interface TEdit {
-    section?: string;
+    /**Name of the group for nested objects within an entity.
+     * Set to 'root' if the property is on the root level
+     */
+    section: string;
+    
+    /**Name of the property to be edited*/
     id: string;
+    
+    /**Value to be saved */
     value: any;
 }
 
 export interface IBaseWriteHook<T> {
-    current: T;
-    edit: (property: TEdit) => Promise<void>;
-    save: () => Promise<void>;
-    remove: () => Promise<void>
+    /**Contains an array of members and properties owned by the given 
+     * type T.
+     */
+    information: IFormField<keyof T>[];
+    
+    /**Actual object writted */
+    object: T;
+    
+    /**Displays any error from the store or from the 
+     * controller hook
+     */
+    error: string | null;
+    
+    /**Flags the loading status of the store and the controller
+     * hook
+     */
+    isLoading: boolean;
+
+    /** For n-select properties, options are to be provided from
+     * the controller hook
+     */
+    options?: {
+        [key: string]: string[] | any[];
+    },
+    
+    /** Handles the validation of the object and 
+     * dispatchment to the store.
+     */
+    onSubmitPress: () => Promise<void>;
+    
+    /** ID must be provided to remove an object */
+    onRemovePress: (id: string) => Promise<void>;
+    
+    /** Use to edit the properties of an object. 
+     * Must include the property name, to be saved value, 
+     * and the section name if within a nested group */
+    onUpdatePress: (params: TEdit) => void;
 }
