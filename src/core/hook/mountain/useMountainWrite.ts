26a3ebcfbb4a082f44dfb1e6aa349a666bd470c9
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-06 08:59:39 +0800

    modified: moved vallidation from store to hook controller

--- src/core/hook/mountain/useMountainWrite.ts
+++ src/core/hook/mountain/useMountainWrite.ts
@@ -1,46 +1,110 @@
-import { MOUNTAIN_INFORMATION } from "@/src/fields/mountainFields";
+import { OPTIONS } from "@/src/constants/constants";
+import { IBaseWriteHook, TEdit } from "@/src/core/interface/domainHookInterface";
+import { Mountain } from "@/src/core/models/Mountain/Mountain";
+import { validate } from "@/src/core/utility/validate";
+import { MountainUIConfig } from "@/src/fields/mountainFields";
 import { router } from "expo-router";
-import { useEffect } from "react";
+import { useState } from "react";
 import { useMountainsStore } from "../../stores/mountainsStore";
-import { MountainParams } from "./useMountainDomain";
 
-export default function useMountainWrite(params: MountainParams){
+export interface IUseMountainWriteParams {
+    mountainId?: string; 
+}
+
+export interface IUseMountainWrite extends IBaseWriteHook<Mountain> {}
+
+export default function useMountainWrite(params: IUseMountainWriteParams): IUseMountainWrite{
     const { mountainId } = params;
 
-    const information = MOUNTAIN_INFORMATION;
-
-    const mountain = useMountainsStore(s => s.current);
+    const mountains = useMountainsStore(s => s.data);
+    const information = MountainUIConfig;
     const error = useMountainsStore(s => s.error);
     const isLoading = useMountainsStore(s => s.isLoading);
+    
+    const [localError, setLocalError] = useState<string | null>(null);
+    const [mountain, setMountain] = useState<Mountain>(() => {
+        const existing = mountains.find(m => m.id === mountainId);
+        return existing ? new Mountain({...existing}) : new Mountain();
+    })
 
-    const loadMountain = useMountainsStore(s => s.load);
+    const options = {
+        provinces: [...OPTIONS.provinces],
+    }
+
     const create = useMountainsStore(s => s.create);
     const edit = useMountainsStore(s => s.edit);
     const remove = useMountainsStore(s => s.delete);
 
-    useEffect(() => {
-        loadMountain(mountainId);
-    },[mountainId])
+    const onSubmitPress = async () => {
+        setLocalError(null)
+        console.log(mountain);
 
-    const onSubmit = async () => {
-        const success = await create();
-        if(!success) return;
-        router.back();
+        try {
+            const errors = validate(mountain, information);
+            
+            if(errors.length > 0) 
+                throw new Error(`${errors.join(', ')} missing`)
+
+            console.log('Create: ', mountain)
+            const created = await create(mountain);
+            
+            if(created) router.back();
+        } catch (error: any) {
+            setLocalError(error instanceof Error ? error.message : 'Failed submitting data')
+        }
     }
 
-    const onDelete = async () => {
-        if(mountainId) await remove(mountainId);
-        router.back();
+    const onRemovePress = async () => {
+        // if(mountainId) await remove(mountainId);
+        // router.back();
+    }
+
+    const onUpdatePress = (params: TEdit) => {
+        const { section, id, value} = params;
+        try {
+            if(section !== 'root' && !id)
+                throw new Error(`Missing key for ${section}`);
+            
+            let finalValue = value;
+
+            const fieldConfig = information.find(f => f.section === section && f.id === id);
+
+            if (fieldConfig?.type === 'multi-select') {
+                const current: [] = section === 'root'
+                    ? mountain[id]
+                    : mountain[section][id]
+
+                finalValue = current.find(c => c === value)
+                    ? current.filter(c => c !== value)
+                    : [...current, value]
+            }
+            
+            setMountain(prev => {
+                return section === 'root'
+                    ? new Mountain({
+                        ...prev,
+                        [id]: finalValue
+                    })
+                    : new Mountain({
+                        ...prev,
+                        [section]: { ...(prev[section] as object), [id]: finalValue}
+                    })
+            })
+
+        } catch (error) {
+            setLocalError(error instanceof Error ? error.message : 'Failed saving trail');   
+        }
     }
 
     return {
-        mountain,
+        object: mountain,
         information,
-        error,
+        error: error || localError,
         isLoading,
-        onEditProperty: edit,
-        onSubmit,
-        onDelete,
+        options,
+        onUpdatePress,
+        onSubmitPress,
+        onRemovePress,
     }
 
 }
