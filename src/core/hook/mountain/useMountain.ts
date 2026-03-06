5d7c16c6da56b20fc7b62600ea2654d0d5c5a40f
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-06 08:58:51 +0800

    modified: revised name to follow convention

--- src/core/hook/mountain/useMountain.ts
+++ src/core/hook/mountain/useMountain.ts
@@ -2,22 +2,17 @@ import { router } from "expo-router";
 import { useEffect } from "react";
 import { useMountainsStore } from "../../stores/mountainsStore";
 
-export type MountainParams = {
-    mountainId: string | null;
-}
-
-export default function useMountainDomain(params: MountainParams){
+export default function useMountain(){
     const mountains = useMountainsStore(s => s.data);
     const isLoading = useMountainsStore(s => s.isLoading);
 
     const loadAllMountains = useMountainsStore(s => s.fetchAll);
-    const load = useMountainsStore(s => s.load);
 
     useEffect(() => {
         loadAllMountains();
-    },[])
+    }, [])
 
-    const onWritePress = (mountainId: string | null) => {
+    const onWritePress = (mountainId?: string | null) => {
         if(mountainId){
             router.push({
                 pathname: '/(main)/superadmin/mountain/write',
