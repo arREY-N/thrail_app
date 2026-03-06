94fde2d0e9e70ac8109dfefbb38b84a74d91493a
Specter1031 <Ejcaraig1031@gmail.com>
2026-03-06 12:43:36 +0800

    modified: modify scroll handling in Privacy and Terms content components

--- src/features/Auth/screens/PrivacyScreen.jsx
+++ src/features/Auth/screens/PrivacyScreen.jsx
@@ -9,41 +9,55 @@ import ScreenWrapper from '@/src/components/ScreenWrapper';
 import { Colors } from '@/src/constants/colors';
 import { AuthStyles } from '@/src/features/Auth/styles/AuthStyles';
 
-export const PrivacyContent = () => (
-    <View style={styles.contentContainer}>
-        <ResponsiveScrollView>
-            <CustomText variant="body" style={styles.legalText}>
-                [PRIVACY POLICY]
-                {'\n'}{'\n'}
-                1. Data Collection{'\n'}
-                We collect information to provide better services to all our users.
-                {'\n'}{'\n'}
-                2. Data Usage{'\n'}
-                Your data is used solely for improving your hiking experience.
-                {'\n'}{'\n'}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                {'\n'}{'\n'}
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                {'\n'}{'\n'}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                {'\n'}{'\n'}
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                {'\n'}{'\n'}
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                {'\n'}{'\n'}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                {'\n'}{'\n'}
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                {'\n'}{'\n'}
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                {'\n'}{'\n'}
-                [End of Privacy]
-            </CustomText>
-        </ResponsiveScrollView>
-    </View>
-);
+export const PrivacyContent = ({ onScrollToBottom }) => {
+    
+    const handleScroll = (event) => {
+        const { layoutMeasurement, contentOffset, contentSize } = event.nativeEvent;
+        const paddingToBottom = 20; 
+        if (layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom) {
+            if (onScrollToBottom) onScrollToBottom();
+        }
+    };
+
+    return (
+        <View style={styles.contentContainer}>
+            <ResponsiveScrollView 
+                onScroll={handleScroll} 
+                scrollEventThrottle={16}
+            >
+                <CustomText variant="body" style={styles.legalText}>
+                    [PRIVACY POLICY]
+                    {'\n'}{'\n'}
+                    1. Data Collection{'\n'}
+                    We collect information to provide better services to all our users.
+                    {'\n'}{'\n'}
+                    2. Data Usage{'\n'}
+                    Your data is used solely for improving your hiking experience.
+                    {'\n'}{'\n'}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {'\n'}{'\n'}
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    {'\n'}{'\n'}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    {'\n'}{'\n'}
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {'\n'}{'\n'}
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    {'\n'}{'\n'}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    {'\n'}{'\n'}
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {'\n'}{'\n'}
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    {'\n'}{'\n'}
+                    [End of Privacy]
+                </CustomText>
+            </ResponsiveScrollView>
+        </View>
+    );
+};
 
 const PrivacyScreen = ({ onBackPress }) => {
     return (
