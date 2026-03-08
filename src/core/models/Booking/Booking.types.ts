05f418156fdb1ce5dfe29caf17e30438082a6c86
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-08 23:10:33 +0800

    modified: added emergencyContact to booking data

--- src/core/models/Booking/Booking.types.ts
+++ src/core/models/Booking/Booking.types.ts
@@ -2,10 +2,11 @@ import { IBusinessSummary } from "@/src/core/models/Business/Business.types";
 import { IOfferInfo } from "@/src/core/models/Offer/Offer.types";
 import { IPaymentSummary } from "@/src/core/models/Payment/Payment.types";
 import { ITrailSummary } from "@/src/core/models/Trail/Trail.types";
-import { IUserSummary } from "@/src/core/models/User/User.types";
+import { IEmergencyContact, IUserSummary } from "@/src/core/models/User/User.types";
 import { FieldValue, Timestamp } from "firebase/firestore";
 
-export type BookingStatus = 'reserved' | 'approved' | 'paid'  | 'cancelled' | 'refund'
+export type BookingStatus = 'reserved' | 'approved' | 'paid'  | 'cancelled' | 'refund' | 'reschedule' | 'pending';
+
 
 export interface IBookingBase<T> {
     id: string;
@@ -16,6 +17,7 @@ export interface IBookingBase<T> {
     payment: IPaymentSummary<T>[];
     status: BookingStatus
     cancelledBy: string | null;
+    emergencyContact: IEmergencyContact;
 }
 
 export interface IBookingDB extends IBookingBase<Timestamp | FieldValue> {}
