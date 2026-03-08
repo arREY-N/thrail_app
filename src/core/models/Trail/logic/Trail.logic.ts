3b77d291308d3d291f2afab6107b25ff385f79eb
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2026-03-08 23:11:54 +0800

    modified: added toSummary functions

---
+++ src/core/models/Trail/logic/Trail.logic.ts
@@ -0,0 +1,11 @@
+import { Trail } from "@/src/core/models/Trail/Trail";
+import { ITrailSummary } from "@/src/core/models/Trail/Trail.types";
+
+export const TrailLogic = {
+    toSummary(trail: Trail): ITrailSummary {
+        return {
+            id: trail.id,
+            name: trail.general.name
+        }
+    }
+}
