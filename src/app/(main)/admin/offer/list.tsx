1776881db3141cba52d08dc6e957048cd98e0e34
Specter1031 <Ejcaraig1031@gmail.com>
2026-03-23 09:53:34 +0800

    modified: add styling in the OfferLsit

--- src/app/(main)/admin/offer/list.tsx
+++ src/app/(main)/admin/offer/list.tsx
@@ -1,14 +1,16 @@
 import useAdminOffer from "@/src/core/hook/admin/useAdminOffer";
 import useAdminNavigation from "@/src/core/hook/navigation/useAdminNavigation";
-import { toDate } from "@/src/core/utility/date";
+import { useAppNavigation } from "@/src/core/hook/navigation/useAppNavigation";
 import getSearchParam from "@/src/core/utility/getSearchParam";
-import { useLocalSearchParams } from "expo-router";
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import OfferListScreen from "@/src/features/Admin/screens/OfferListScreen";
+import { Stack, useLocalSearchParams } from "expo-router";
+import React from "react";
 
 export default function adminOfferList(){
     const { businessId: rawId } = useLocalSearchParams();
-    
     const id = getSearchParam(rawId);
+
+    const { onBackPress } = useAppNavigation();
 
     const {
         onWriteOffer,
@@ -21,49 +23,63 @@ export default function adminOfferList(){
     } = useAdminOffer({ businessId: id });
 
     return (
-        <ScrollView>
-            <Pressable onPress={() => onWriteOffer()}>
-                <Text>ADD NEW</Text>
-            </Pressable>
+        <>
+            <Stack.Screen options={{ headerShown: false }} />
+            
+            <OfferListScreen 
+                offers={businessOffers}
+                isLoading={isLoading}
+                onAddOffer={onWriteOffer} 
+                onEditOffer={onWriteOffer}
+                onBackPress={onBackPress}
+            />
+        </>
+    );
 
-            { isLoading 
-                ? <Text>LOADING OFFERS</Text>  
-                : <View>
-                    { businessOffers.length > 0
-                        ? businessOffers.map(o => {
-                            return (
-                                <View style={styles.offerForm} key={o.id}>
-                                    <Text>Trail: {o.trail.name}</Text>
-                                    <Text>Price: P{o.price}.00 </Text>
-                                    <Text>Date: {toDate(o.hikeDate).toDateString()}</Text>
-                                    <Text>Duration: {o.hikeDuration}</Text>
-                                    <Text>Documents: {o.documents?.join(', ')}</Text>
-                                    <Text>Inclusions: {o.inclusions.length > 0 ? o.inclusions?.join(', ') : 'None'}</Text>
-                                    <Text>Description: {o.description}</Text>
+    // return (
+    //     <ScrollView>
+    //         <Pressable onPress={() => onWriteOffer()}>
+    //             <Text>ADD NEW</Text>
+    //         </Pressable>
+
+    //         { isLoading 
+    //             ? <Text>LOADING OFFERS</Text>  
+    //             : <View>
+    //                 { businessOffers.length > 0
+    //                     ? businessOffers.map(o => {
+    //                         return (
+    //                             <View style={styles.offerForm} key={o.id}>
+    //                                 <Text>Trail: {o.trail.name}</Text>
+    //                                 <Text>Price: P{o.price}.00 </Text>
+    //                                 <Text>Date: {toDate(o.hikeDate).toDateString()}</Text>
+    //                                 <Text>Duration: {o.hikeDuration}</Text>
+    //                                 <Text>Documents: {o.documents?.join(', ')}</Text>
+    //                                 <Text>Inclusions: {o.inclusions.length > 0 ? o.inclusions?.join(', ') : 'None'}</Text>
+    //                                 <Text>Description: {o.description}</Text>
                                     
-                                    <Pressable onPress={() => onWriteOffer(o.id)}>
-                                        <Text>Edit Offer</Text>
-                                    </Pressable>
-                                </View>
-                            )})
+    //                                 <Pressable onPress={() => onWriteOffer(o.id)}>
+    //                                     <Text>Edit Offer</Text>
+    //                                 </Pressable>
+    //                             </View>
+    //                         )})
                             
-                        : <Text style={styles.loading}>No Offers</Text>
-                    }
-                </View> 
-            }
-        </ScrollView>
-    )
+    //                     : <Text style={styles.loading}>No Offers</Text>
+    //                 }
+    //             </View> 
+    //         }
+    //     </ScrollView>
+    // )
 }
 
-const styles = StyleSheet.create({
-    offerForm: {
-        borderWidth: 1,
-        margin: 10,
-        padding: 5
-    },
-    loading: {
-        textAlign: 'center',
-        margin: 10,
-        padding: 10,
-    }
-})
+// const styles = StyleSheet.create({
+//     offerForm: {
+//         borderWidth: 1,
+//         margin: 10,
+//         padding: 5
+//     },
+//     loading: {
+//         textAlign: 'center',
+//         margin: 10,
+//         padding: 10,
+//     }
+// })
