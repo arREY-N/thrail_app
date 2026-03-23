c1b808cabe2d160700b855eac3f1c9e015974147
Reyn Penus <reypenus1224@gmail.com>
2026-03-23 10:55:12 +0800

    modified: removed unused abstract class

--- src/core/interface/storeInterface.ts
+++ src/core/interface/storeInterface.ts
@@ -1,5 +1,4 @@
 import { TEdit } from "@/src/core/interface/domainHookInterface";
-import { Property } from "@/src/core/types/Property";
 
 export interface BaseStore<T>{
     data: T[];
@@ -8,32 +7,11 @@ export interface BaseStore<T>{
     error: string | null;
 
     fetchAll: () => Promise<void>;
-    refresh: () => Promise<void>;
+    refresh: (userId?: string | null) => Promise<void>;
     load: (...args: any) => Promise<void>;
     create: (...args: any) => Promise<Boolean>;
     delete: (id: string) => Promise<void>;
-    edit?: (property: TEdit) => void;
+    edit?: (property: TEdit<T>) => void;
     reset: () => void;
 }
 
-export abstract class Store<T> implements BaseStore<T>{
-    data: T[];
-    current: T | null;
-    isLoading: boolean;
-    error: string | null;
-    
-    constructor(){
-        this.data = [];
-        this.current = null;
-        this.isLoading = true;
-        this.error = null;
-    }
-    
-    abstract fetchAll(): Promise<void>;
-    abstract refresh(): Promise<void>;
-    abstract load(...args: any[]): Promise<void>;
-    abstract create(...args: any[]): Promise<Boolean>;
-    abstract delete(...args: any[]): Promise<void>;
-    abstract edit(property: Property | TEdit): void;
-    abstract reset(): void;
-}
