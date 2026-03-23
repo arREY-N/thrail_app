6916c8d0f437a0141a13512d7f653a874af56d7d
Reyn Penus <reypenus1224@gmail.com>
2026-03-23 10:53:41 +0800

    deleted: removed unused file

--- src/core/hook/book/useBookDomain.ts
+++
@@ -1,9 +0,0 @@
-import { useOffersStore } from "../../stores/offersStore";
-
-export default function useBookDomain(){
-    const offer = useOffersStore(s => s.current);
-
-    const onBookPress = (trailId: string) => {
-        console.log('booking', trailId);
-    }
-}
