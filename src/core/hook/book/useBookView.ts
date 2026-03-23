7df18056d25df199aa8ec47ee576c376c56bee0f
Reyn Penus <reypenus1224@gmail.com>
2026-03-23 10:54:54 +0800

    modified: clean up of unused functions

--- src/core/hook/book/useBookView.ts
+++ src/core/hook/book/useBookView.ts
@@ -2,9 +2,7 @@ import { useAuthStore } from "@/src/core/stores/authStore";
 import useBookingsStore from "@/src/core/stores/bookingsStore";
 import { useOffersStore } from "@/src/core/stores/offersStore";
 import { usePaymentsStore } from "@/src/core/stores/paymentsStore";
-import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { Offer } from "../../models/Offer/Offer";
 
 export type BookParams = {
     offerId: string | null;
@@ -17,16 +15,13 @@ export function useBookView(params: BookParams){
     const systemPayments = usePaymentsStore(s => s.error);
     const systemBookings = useBookingsStore(s => s.error);
 
-    const router = useRouter();    
     const profile = useAuthStore(s => s.profile);
 
     const loadOffer = useOffersStore(s => s.load);
     const offer = useOffersStore(s => s.current);
 
-    const createPayment = usePaymentsStore(s => s.create);
     const paymentIsLoading = usePaymentsStore(s => s.isLoading);
     
-    const createBooking = useBookingsStore(s => s.createBooking);
     const bookingIsLoading = useBookingsStore(s => s.isLoading);
 
     const [mode, setMode] = useState('');
@@ -34,56 +29,17 @@ export function useBookView(params: BookParams){
 
     const userBookings = useBookingsStore(s => s.userBookings);
     const bookingErrors = useBookingsStore(s => s.error);
-    const cancelBooking = useBookingsStore(s => s.cancelBooking);
     
     useEffect(() => {
         console.log('loading: ', offerId);
         loadOffer({ id: offerId })
     }, [offerId]);
 
-    function onCancelBookingPress(bookingData: Offer){
-        try {
-            if(!profile) throw new Error('Profile is empty');
-
-            cancelBooking({
-                cancelledBy: profile.role,
-                bookingData
-            });
-        } catch (err) {
-            console.log((err as Error).message);
-        }
-    }
-
-    const onPayPress = async (mode: string) => {
-        const payment = await createPayment({
-            profile,
-            offer,
-            mode
-        })
-
-        if(!payment) return;
-
-        await createBooking({
-            bookingData: {
-                payment: 
-                offer
-            },
-        });
-
-        // router.push({
-        //     pathname: '/receipt/view',
-        //     params: { paymentId: payment.id }
-        // });
-    }
-
-    console.log(offer);
-
     return {
         offer,
         mode,
         setMode,
         modes,
-        onPayPress,
         paymentIsLoading,
         bookingIsLoading,
         systemBookings,
@@ -92,6 +48,5 @@ export function useBookView(params: BookParams){
         profile,
         userBookings,
         bookingErrors,
-        onCancelBookingPress
     }
 }
