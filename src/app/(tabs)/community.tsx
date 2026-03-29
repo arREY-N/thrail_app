e7f0f071c860e6187ba8affc9620abe4f32f328b
Reyn Penus <reypenus1224@gmail.com>
2026-03-30 02:35:27 +0800

    modified: testing view of review objects

--- src/app/(tabs)/community.tsx
+++ src/app/(tabs)/community.tsx
@@ -1,42 +1,93 @@
-import { router } from 'expo-router'
-import React from 'react'
-import { Pressable, Text, View } from 'react-native'
+import useReview from '@/src/core/hook/review/useReview';
+import { Review } from '@/src/core/models/Review/Review';
+import React from 'react';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function community(){
 
-    const onWriteReviewPress = (id?: string) => {
-        if(id){
-            router.push({
-                pathname: '/(main)/review/write',
-                params: { reviewId: id }
-             })
-        } else {
-            router.push({
-                pathname: '/(main)/review/write',
-            })
-        }
-    }
+    const {
+        reviews,
+        onWriteReviewPress,
+        isOwned,
+        likeReview,
+        isLiked,
+        refreshFeed,
+    } = useReview();
 
     return (
         <TestCommunityScreen 
-            onWriteReviewPress={onWriteReviewPress}/>
+            onWriteReviewPress={onWriteReviewPress}
+            reviews={reviews}
+            isOwned={isOwned}
+            likeReview={likeReview}
+            isLiked={isLiked}
+            refreshFeed={refreshFeed}
+        />
     )
 }
 
 export type CommunityScreenParams = {
+    reviews: Review[];
+
     onWriteReviewPress: (id?: string) => void;
+    isOwned: (review: Review) => Boolean;
+    likeReview: (review: Review) => void;
+    isLiked: (review: Review) => Boolean;
+    refreshFeed: () => void;
 }
 
 export const TestCommunityScreen = (params: CommunityScreenParams) => {
-    const { onWriteReviewPress } = params;
+    const { 
+        onWriteReviewPress, 
+        reviews,
+        isOwned,
+        likeReview,
+        isLiked,
+        refreshFeed
+    } = params;
 
     return(
-        <View>
+        <ScrollView>
             <Text>Test Community Screen</Text>
 
             <Pressable onPress={() => onWriteReviewPress()}>
                 <Text>Create New Review</Text>
             </Pressable>
-        </View>
+            
+            <Pressable onPress={() => refreshFeed()}>
+                <Text>Refresh</Text>
+            </Pressable>
+
+            {
+                reviews.map(r => (
+                    <View style={styles.review} key={r.id}>
+                            <Text>{r.hike.review}</Text>
+                            <Text>{r.likes.length}</Text>
+                            <Pressable onPress={() => likeReview(r)}>
+                                { isLiked(r) 
+                                    ? <Text>Unlike review</Text> 
+                                    : <Text>Like review</Text> 
+                                } 
+                            </Pressable>
+                            {
+                                isOwned(r) && 
+                                <Pressable key={r.id} onPress={() => onWriteReviewPress(r.id)}>
+                                    <Text>Edit My Review</Text>
+                                </Pressable>
+                            }
+                    </View>
+                ))
+            }
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    review: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        padding: 12,
+        margin: 10
+    }
+})
