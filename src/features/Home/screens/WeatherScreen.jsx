8d563c8cabb3c6602665d1dc1d41296d5dd366c3
Specter1031 <Ejcaraig1031@gmail.com>
2026-03-30 04:25:39 +0800

    modified: modify the styling, and separate the code

--- src/features/Home/screens/WeatherScreen.jsx
+++ src/features/Home/screens/WeatherScreen.jsx
@@ -1,11 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import {
-    Animated,
-    Easing,
     Platform,
     RefreshControl,
     StyleSheet,
-    TouchableOpacity,
     View
 } from 'react-native';
 
@@ -17,8 +14,10 @@ import ScreenWrapper from '@/src/components/ScreenWrapper';
 
 import { Colors } from '@/src/constants/colors';
 
+import WeatherSkeleton from '@/src/features/Home/components/WeatherSkeleton';
+
 const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetching }) => {
-    console.log("RECEIVED DATA:", locationWeather);
+    
     const [testLoading, setTestLoading] = useState(true);
     const [refreshing, setRefreshing] = useState(false);
 
@@ -43,102 +42,15 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
     const uvIndex = locationWeather?.UV ? String(locationWeather.UV) : "--";
 
     const onRefresh = useCallback(async () => {
-        console.log("Pull-to-refresh triggered!");
         setRefreshing(true);
-
         if (onRefreshPress) {
-            console.log("Calling backend refresh function...");
             await onRefreshPress(); 
         }
         setRefreshing(false);
-        console.log("Refresh loading stopped.");
     }, [onRefreshPress]);
 
-    const WebRefreshButton = Platform.OS === 'web' ? (
-        <TouchableOpacity onPress={onRefresh} style={{ padding: 8 }}>
-            <CustomIcon 
-                library="Ionicons" 
-                name="refresh" 
-                size={24} 
-                color={Colors.TEXT_INVERSE} 
-            />
-        </TouchableOpacity>
-    ) : null;
-
     if (isLoading) {
-        return (
-            <ScreenWrapper backgroundColor={Colors.BACKGROUND}>
-                <CustomHeader title="Weather" onBackPress={onBackPress} />
-                <View style={styles.scrollContent}>
-                    <View style={styles.heroSection}>
-                        <View style={styles.heroTop}>
-                            <SkeletonEffect style={{ width: 120, height: 80 }} />
-                            <SkeletonEffect style={{ width: 96, height: 96, borderRadius: 48 }} />
-                        </View>
-                        <SkeletonEffect style={{ width: '100%', height: 2, marginVertical: 16 }} />
-                        <View style={styles.heroBottom}>
-                            <SkeletonEffect style={{ width: 100, height: 20 }} />
-                            <SkeletonEffect style={{ width: 150, height: 20 }} />
-                        </View>
-                    </View>
-                    
-                    <View style={styles.fullWidthCard}>
-                        <View style={styles.forecastRow}>
-                            {[1, 2, 3, 4].map((i) => (
-                                <View key={i} style={styles.fItem}>
-                                    <SkeletonEffect style={{ width: 40, height: 60, borderRadius: 20 }} />
-                                    <SkeletonEffect style={{ width: 30, height: 15 }} />
-                                </View>
-                            ))}
-                        </View>
-                    </View>
-
-                    <View style={styles.fullWidthCard}>
-                        <View style={styles.forecastRow}>
-                            {[1, 2, 3, 4].map((i) => (
-                                <View key={i} style={styles.fItem}>
-                                    <SkeletonEffect style={{ width: 40, height: 40, borderRadius: 20 }} />
-                                    <SkeletonEffect style={{ width: 30, height: 15 }} />
-                                </View>
-                            ))}
-                        </View>
-                    </View>
-
-                    <View style={styles.fullWidthCard}>
-                        <View style={styles.forecastRow}>
-                            {[1, 2, 3, 4].map((i) => (
-                                <View key={i} style={styles.fItem}>
-                                    <SkeletonEffect style={{ width: 40, height: 40, borderRadius: 20 }} />
-                                    <SkeletonEffect style={{ width: 30, height: 15 }} />
-                                </View>
-                            ))}
-                        </View>
-                    </View>
-
-                    <View style={styles.fullWidthCard}>
-                        <View style={styles.forecastRow}>
-                            {[1, 2, 3, 4].map((i) => (
-                                <View key={i} style={styles.fItem}>
-                                    <SkeletonEffect style={{ width: 40, height: 40, borderRadius: 20 }} />
-                                    <SkeletonEffect style={{ width: 30, height: 15 }} />
-                                </View>
-                            ))}
-                        </View>
-                    </View>
-
-                    <View style={styles.fullWidthCard}>
-                        <View style={styles.forecastRow}>
-                            {[1, 2, 3, 4].map((i) => (
-                                <View key={i} style={styles.fItem}>
-                                    <SkeletonEffect style={{ width: 40, height: 40, borderRadius: 20 }} />
-                                    <SkeletonEffect style={{ width: 30, height: 15 }} />
-                                </View>
-                            ))}
-                        </View>
-                    </View>
-                </View>
-            </ScreenWrapper>
-        );
+        return <WeatherSkeleton onBackPress={onBackPress} />;
     }
 
     return (
@@ -147,7 +59,6 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
             <CustomHeader 
                 title="Weather" 
                 onBackPress={onBackPress} 
-                rightActions={WebRefreshButton}
             />
 
             <ResponsiveScrollView 
@@ -168,20 +79,17 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
                                 <CustomText variant="title" style={styles.mainTemp}>
                                     {temperature}°
                                 </CustomText>
-
                                 <CustomText variant="subtitle" style={styles.tempUnit}>
                                     C
                                 </CustomText>
                             </View>
                             <View style={styles.locationRow}>
-
                                 <CustomIcon 
                                     library="FontAwesome6" 
                                     name="location-dot" 
                                     size={16} 
                                     color={Colors.BLACK} 
                                 />
-
                                 <CustomText variant="body" style={styles.locationLabel}>
                                     {location}
                                 </CustomText>
@@ -202,7 +110,6 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
                         <CustomText variant="body" style={styles.heroSubText}>
                             Mostly Cloudy
                         </CustomText>
-
                         <CustomText variant="body" style={styles.heroSubText}>
                             Day: {dayTemp}° | Night: {nightTemp}°
                         </CustomText>
@@ -211,7 +118,6 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
 
                 <View style={styles.fullWidthCard}>
                     <View style={styles.cardHeader}>
-
                         <CustomIcon 
                             library="Ionicons" 
                             name="calendar" 
@@ -231,46 +137,20 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
                 </View>
 
                 <View style={styles.bentoGrid}>
-                    <BentoBox 
-                        title="Wind" 
-                        value={wind}
-                        desc="From Southeast" 
-                        icon="wind" 
-                        lib="Feather" 
-                    />
-                    <BentoBox 
-                        title="Precipitation" 
-                        value={precipAmount}
-                        desc={`${precipChance} chance`} 
-                        icon="rainy" 
-                        lib="Ionicons" 
-                    />
-                    <BentoBox 
-                        title="UV Index" 
-                        value={uvIndex} 
-                        desc="Moderate" 
-                        icon="sun" 
-                        lib="Feather" 
-                    />
-                    <BentoBox 
-                        title="Humidity" 
-                        value={humidity}
-                        desc="Humid air" 
-                        icon="water" 
-                        lib="Ionicons" 
-                    />
+                    <BentoBox title="Wind" value={wind} desc="From Southeast" icon="wind" lib="Feather" />
+                    <BentoBox title="Precipitation" value={precipAmount} desc={`${precipChance} chance`} icon="rainy" lib="Ionicons" />
+                    <BentoBox title="UV Index" value={uvIndex} desc="Moderate" icon="sun" lib="Feather" />
+                    <BentoBox title="Humidity" value={humidity} desc="Humid air" icon="water" lib="Ionicons" />
                 </View>
 
                 <View style={styles.fullWidthCard}>
                     <View style={styles.cardHeader}>
-
                         <CustomIcon 
                             library="Ionicons" 
                             name="sunny" 
                             size={18} 
                             color={Colors.TEXT_SECONDARY} 
                         />
-
                         <CustomText variant="body" style={styles.cardHeaderTitle}>
                             Sun
                         </CustomText>
@@ -278,37 +158,15 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
                     
                     <View style={styles.sunTimeRow}>
                         <View style={styles.sunItem}>
-
-                            <CustomIcon 
-                                library="Feather" 
-                                name="sunrise" 
-                                size={32} 
-                                color={Colors.BLACK} 
-                            />
-                            <CustomText variant="subtitle" style={styles.sunTimeText}>
-                                6:02 AM
-                            </CustomText>
-
-                            <CustomText variant="caption" style={styles.sunLabel}>
-                                Sunrise
-                            </CustomText>
+                            <CustomIcon library="Feather" name="sunrise" size={32} color={Colors.BLACK} />
+                            <CustomText variant="subtitle" style={styles.sunTimeText}>6:02 AM</CustomText>
+                            <CustomText variant="caption" style={styles.sunLabel}>Sunrise</CustomText>
                         </View>
 
                         <View style={[styles.sunItem, { alignItems: 'flex-end' }]}>
-
-                            <CustomIcon 
-                                library="Feather" 
-                                name="sunset" 
-                                size={32} 
-                                color={Colors.BLACK} 
-                            />
-                            <CustomText variant="subtitle" style={styles.sunTimeText}>
-                                6:15 PM
-                            </CustomText>
-
-                            <CustomText variant="caption" style={styles.sunLabel}>
-                                Sunset
-                            </CustomText>
+                            <CustomIcon library="Feather" name="sunset" size={32} color={Colors.BLACK} />
+                            <CustomText variant="subtitle" style={styles.sunTimeText}>6:15 PM</CustomText>
+                            <CustomText variant="caption" style={styles.sunLabel}>Sunset</CustomText>
                         </View>
                     </View>
                 </View>
@@ -318,59 +176,16 @@ const WeatherScreen = ({ locationWeather, onBackPress, onRefreshPress, isFetchin
     );
 };
 
-const SkeletonEffect = ({ style }) => {
-    const opacity = React.useRef(new Animated.Value(0.3)).current;
-
-    React.useEffect(() => {
-        Animated.loop(
-            Animated.sequence([
-                Animated.timing(opacity, {
-                    toValue: 1,
-                    duration: 800,
-                    easing: Easing.inOut(Easing.ease),
-                    useNativeDriver: true,
-                }),
-                Animated.timing(opacity, {
-                    toValue: 0.3,
-                    duration: 800,
-                    easing: Easing.inOut(Easing.ease),
-                    useNativeDriver: true,
-                }),
-            ])
-        ).start();
-    }, [opacity]);
-
-    return <Animated.View style={[{ backgroundColor: Colors.GRAY_LIGHT, borderRadius: 8 }, style, { opacity }]} />;
-};
-
 const ForecastItem = ({ day, icon, low, high }) => (
     <View style={styles.fItem}>       
         <View style={styles.fIconWrapper}>
-
-            <CustomIcon 
-                library="MaterialCommunityIcons" 
-                name={icon} 
-                size={32} 
-                color={Colors.BLACK} 
-            />
+            <CustomIcon library="MaterialCommunityIcons" name={icon} size={32} color={Colors.BLACK} />
         </View>
-
-        <CustomText variant="caption" style={styles.fDay}>
-            {day}
-        </CustomText>
-
+        <CustomText variant="caption" style={styles.fDay}>{day}</CustomText>
         <View style={styles.fTempRow}>
-            <CustomText variant="body" style={styles.fTempHigh}>
-                {high}°
-            </CustomText>
-
-            <CustomText style={styles.fTempSeparator}>
-                /
-            </CustomText>
-
-            <CustomText variant="caption" style={styles.fTempLow}>
-                {low}°
-            </CustomText>
+            <CustomText variant="body" style={styles.fTempHigh}>{high}°</CustomText>
+            <CustomText style={styles.fTempSeparator}>/</CustomText>
+            <CustomText variant="caption" style={styles.fTempLow}>{low}°</CustomText>
         </View> 
     </View>
 );
@@ -378,30 +193,30 @@ const ForecastItem = ({ day, icon, low, high }) => (
 const BentoBox = ({ title, value, desc, icon, lib }) => (
     <View style={styles.bentoBox}>
         <View style={styles.bentoHeader}>
-
-            <CustomIcon 
-                library={lib}
-                name={icon}
-                size={18} 
-                color={Colors.TEXT_SECONDARY} 
-            />
-            
-            <CustomText variant="body" style={styles.bentoTitle}>
-                {title}
-            </CustomText>
+            <CustomIcon library={lib} name={icon} size={18} color={Colors.TEXT_SECONDARY} />
+            <CustomText variant="body" style={styles.bentoTitle}>{title}</CustomText>
         </View>
-
         <View>
-            <CustomText variant="subtitle" style={styles.bentoValue}>
-                {value}
-            </CustomText>
-
-            <CustomText variant="caption" style={styles.bentoDesc}>
-                {desc}
-            </CustomText>
+            <CustomText variant="subtitle" style={styles.bentoValue}>{value}</CustomText>
+            <CustomText variant="caption" style={styles.bentoDesc}>{desc}</CustomText>
         </View>
     </View>
 );
+
+const dropShadow = Platform.select({
+    ios: {
+        shadowColor: Colors.SHADOW,
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.08,
+        shadowRadius: 12,
+    },
+    android: {
+        elevation: 3,
+    },
+    web: {
+        boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.08)',
+    }
+});
 
 const styles = StyleSheet.create({
     container: {
@@ -412,7 +227,6 @@ const styles = StyleSheet.create({
         gap: 16,
         paddingBottom: 32,
     },
-
     heroSection: {
         paddingVertical: 16,
     },
@@ -466,10 +280,11 @@ const styles = StyleSheet.create({
     },
 
     fullWidthCard: {
-        backgroundColor: Colors.SECONDARY,
+        backgroundColor: Colors.WHITE, 
         borderRadius: 16,
         padding: 16,
         gap: 16,
+        ...dropShadow, 
     },
     cardHeader: {
         flexDirection: 'row',
@@ -486,7 +301,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         alignItems: 'center',
     },
-    
     fItem: {
         alignItems: 'center',
         gap: 4,
@@ -505,7 +319,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-
     fTempHigh: {
         fontWeight: '800',
         color: Colors.TEXT_PRIMARY,
@@ -527,7 +340,7 @@ const styles = StyleSheet.create({
         marginHorizontal: -8,
     },
     bentoBox: {
-        backgroundColor: Colors.SECONDARY,
+        backgroundColor: Colors.WHITE, 
         borderRadius: 16,
         padding: 16,
         margin: 8,
@@ -535,6 +348,7 @@ const styles = StyleSheet.create({
         minWidth: 140,
         minHeight: 160,
         justifyContent: 'space-between',
+        ...dropShadow, 
     },
     bentoHeader: {
         flexDirection: 'row',
@@ -547,7 +361,7 @@ const styles = StyleSheet.create({
     },
     bentoValue: {
         fontWeight: 'bold',
-        marginBottom: 32,
+        marginBottom: 8, 
     },
     bentoDesc: {
         fontWeight: '500',
@@ -571,7 +385,6 @@ const styles = StyleSheet.create({
         color: Colors.TEXT_SECONDARY,
         textTransform: 'uppercase',
     },
-    
 });
 
 export default WeatherScreen;
