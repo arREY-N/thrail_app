7d890c2e46cdb198896cd6318318163476735e82
Zeed111 <veeennn.nn@gmail.com>
2026-04-19 03:59:25 +0800

    add offline fonts for the maps and add jsdocs for trailmap.native, offlinestyle, resolveofflinefonts

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,6 @@ const config = getDefaultConfig(__dirname);
 // Tell Metro to treat .pmtiles and .geojson as valid asset files
 config.resolver.assetExts.push("pmtiles", "geojson");
 
+config.resolver.assetExts.push("pbf");
+
 module.exports = config;
