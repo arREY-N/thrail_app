c9299a8823f4215ad4ad53e0ce182433f1bce103
Reyn Penus <reypenus1224@gmail.com>
2026-04-21 02:26:58 +0800

    modified: added file upload for captured photo

--- src/core/stores/fileStore.ts
+++ src/core/stores/fileStore.ts
@@ -1,4 +1,5 @@
 import { FileRepository } from "@/src/core/repositories/fileRepository";
+import { useAuthStore } from "@/src/core/stores/authStore";
 import * as DocumentPicker from 'expo-document-picker';
 import * as ImagePicker from 'expo-image-picker';
 import { create } from "zustand";
@@ -45,7 +46,13 @@ export const useFilesStore = create<FileState>()(immer((set, get) => ({
 
     capturePhoto: async (): Promise<string> => {
         try {
+            const { profile } = useAuthStore.getState();
+
             const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+            if(!profile) {
+                throw new Error('User profile not found. Please log in again.');
+            }
 
             if (status !== 'granted') {
                 throw new Error('Permission to access media library is required!');
@@ -61,8 +68,22 @@ export const useFilesStore = create<FileState>()(immer((set, get) => ({
                 throw new Error('Photo capture canceled');
             }
 
-            const { uri } = result.assets[0];
-            return uri;
+            const { uri, fileName, mimeType, fileSize: size } = result.assets[0];
+
+            const selectedFile = { 
+                uri, 
+                name: fileName ?? `${profile.firstname}_photo_${Date.now()}.jpg`,
+                mimeType, 
+                size 
+            };
+
+            const downloadURL = await FileRepository.uploadDocuments(selectedFile);
+            
+            if (!downloadURL) {
+                throw new Error('Failed to upload document');
+            }
+
+            return downloadURL;
         } catch (error) {
             throw error instanceof Error ? error : new Error('An unexpected error occurred while requesting permissions.');
         }
