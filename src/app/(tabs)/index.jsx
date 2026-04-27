dd370342fafc7746180532ff023e26e050967be0
Specter1031 <Ejcaraig1031@gmail.com>
2026-04-28 02:07:11 +0800

    modified: add a prop to temporary display the trails of discover

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { useAppNavigation } from '@/src/core/hook/navigation/useAppNavigation';
 
 import useRecommendation from '@/src/core/hook/recommendation/useRecommendation';
+import useTrailDomain from '@/src/core/hook/trail/useTrailDomain';
 import { useAuthHook } from '@/src/core/hook/user/useAuthHook';
 import useWeather from '@/src/core/hook/weather/useWeather';
 import HomeScreen from '@/src/features/Home/screens/HomeScreen';
 
 export default function home(){
     const { profile } = useAuthHook();
+    const { trails } = useTrailDomain();
     
     const { 
         onMountainPress, 
@@ -30,6 +32,11 @@ export default function home(){
         error: errorReco,
         recommendedTrails, 
     } = useRecommendation({userId: profile.id});
+
+    const discoverList = useMemo(() => {
+        if (!trails || !Array.isArray(trails)) return [];
+        return trails.slice(0, 3);
+    }, [trails]);
     
     return (
         <HomeScreen 
@@ -38,7 +45,7 @@ export default function home(){
             onViewAllRecommendationPress={onViewAllRecommendationPress}
             onViewAllTrendingPress={onViewAllTrendingPress}
             recommendedTrails={recommendedTrails}
-            discoverTrails={[]}
+            discoverTrails={discoverList}
             onMountainPress={onMountainPress}
             onDownloadPress={onDownloadPress}
         />
