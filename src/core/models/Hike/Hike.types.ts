68b6a90b33657cccaeb9c57ad1480decf69c29b2
Reyn Penus <reypenus1224@gmail.com>
2026-05-14 14:23:47 +0800

    modified: removed unused code block

--- src/core/models/Hike/Hike.types.ts
+++ src/core/models/Hike/Hike.types.ts
@@ -1,20 +1,7 @@
 import { DifficultyRating, IHikeSurvey } from "@/src/core/models/Review/Review.types";
-import { FieldValue, GeoPoint, Timestamp } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 export type Status = 'unhiked' | 'started' | 'paused' | 'completed'
-
-export interface Coordinates {
-    latitude: number;
-    longitude: number;
-    altitude: number;
-    timestamp: Date;
-}
-
-export interface CoordinatesDB {
-    point: GeoPoint;
-    altitude: number;
-    timestamp: Timestamp | FieldValue;
-}
 
 export interface IHikeBase<T, TRating> extends IHikeSurvey<T, TRating>{
     status: Status;
