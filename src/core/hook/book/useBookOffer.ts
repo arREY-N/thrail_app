01be11d9a3c28e70dba1402850335a5bec89f246
Specter1031 <Ejcaraig1031@gmail.com>
2026-05-14 13:34:52 +0800

    modified: add onRescheduleBooking function placeholder

--- src/core/hook/book/useBookOffer.ts
+++ src/core/hook/book/useBookOffer.ts
@@ -10,10 +10,12 @@ import useBookingsStore from "@/src/core/stores/bookingsStore";
 import { useGroupStore } from "@/src/core/stores/groupStores/groupStoreCreator";
 import { useOffersStore } from "@/src/core/stores/offersStore";
 import { useTrailsStore } from "@/src/core/stores/trailsStore";
+import { formatDateToStandard } from "@/src/utils/dateFormatter";
 import { router } from "expo-router";
 import { httpsCallable } from "firebase/functions";
 import { produce } from "immer";
 import { useEffect, useState } from "react";
+import { Alert, Platform } from "react-native";
 
 export type UseBookOfferParams = {
     bookingId?: string;
@@ -247,6 +249,31 @@ export default function useBookOffer(params: UseBookOfferParams = {}) {
         }
     }
 
+    const onRescheduleBooking = async (bookingToReschedule: Booking, newOffer: Offer) => {
+        try {
+            if(!bookingToReschedule) throw new Error('No booking found');
+            if(!newOffer) throw new Error('A new offer must be provided to reschedule');
+
+            console.log("TODO: Backend Reschedule Integration", {
+                oldBooking: bookingToReschedule,
+                newOffer: newOffer
+            });
+
+            const msg = `Successfully requested to reschedule to ${newOffer.trail?.name || 'a new date'} on ${formatDateToStandard(newOffer.date)}. (Placeholder)`;
+
+            if (Platform.OS === 'web') {
+                window.alert(msg);
+            } else {
+                Alert.alert("Reschedule Requested", msg);
+            }
+
+            router.back();
+
+        } catch (error) {
+            setLocalError((error as Error).message || 'Failed to reschedule booking');
+        }
+    }
+
     return {
         booking,
         bookings,
@@ -258,6 +285,7 @@ export default function useBookOffer(params: UseBookOfferParams = {}) {
         onCompleteBook,
         onCancelBookingPress,
         onRefundBookingPress,
+        onRescheduleBooking,
         getBookOffer,
     }
 }
