4793d61a7a864c813f5dca40289d580ebc36ceff
Specter1031 <Ejcaraig1031@gmail.com>
2026-06-20 11:51:18 +0800

    modified: Refactor preference saving logic to ensure UID is correctly retrieved and handled

--- src/app/(auth)/preference.tsx
+++ src/app/(auth)/preference.tsx
@@ -1,6 +1,7 @@
 import { router } from "expo-router";
 import React, { useState } from "react";
 
+import { auth } from "@/src/core/config/Firebase";
 import { finishOnboarding } from "@/src/core/FirebaseAuthUtil";
 import { useAuthHook } from "@/src/core/hook/user/useAuthHook";
 import usePreference from "@/src/core/hook/user/usePreference";
@@ -21,8 +22,12 @@ export default function Preference() {
     const onFinishedPreference = async () => {
         try {
             const finalData = savePreference(); 
+            const uid = user?.uid || auth.currentUser?.uid;
+            
+            if (!uid) throw new Error("Missing UID. User might not be fully logged in yet.");
+            
             console.log('Trying to save preference and medical profile');
-            await finishOnboarding(user?.uid as string, finalData as unknown as Parameters<typeof finishOnboarding>[1]);
+            await finishOnboarding(uid, finalData as unknown as Parameters<typeof finishOnboarding>[1]);
             router.replace('/(tabs)' as any);
         } catch (err: unknown) {
             setError(err instanceof Error ? err.message : 'An unexpected error occurred');
