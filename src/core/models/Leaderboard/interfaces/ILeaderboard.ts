96f2b1fdd8f9b430c73f787f86f8efa22b907145
Reyn Penus <reypenus1224@gmail.com>
2026-08-04 00:58:26 +0800

    feat(leaderboard): add interfaces for leaderboard and user hiking records

---
+++ src/core/models/Leaderboard/interfaces/ILeaderboard.ts
@@ -0,0 +1,34 @@
+export interface Leaderboard<T> {
+    id: string;
+    date: T;
+    createdAt: T;
+    updatedAt: T;
+    userRankings: RankedUsers<T>[];
+}
+
+export interface RankedUserInfo {
+    userId: string;
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    profileImage: string;
+}
+
+export interface UserHikingRecords<T> extends RankedUserInfo {
+    hikingRecords: HikingRecord<T>[];
+    totalDistance: number;
+    totalElevation: number;
+    totalHikes: number;
+}
+
+export interface RankedUsers<T> extends UserHikingRecords<T> {
+    rank: number;
+}
+
+export interface HikingRecord<T> {
+    hikeId: string;
+    distance: number;
+    elevation: number;
+    hikeDate: T;
+}
