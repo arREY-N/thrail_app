4e205d9db63ee7e60f4875909384605509c31167
Reyn Penus <reypenus1224@gmail.com>
2026-08-05 01:07:37 +0800

    feat(repositories): add OfferRepo initialization in repositories.ts

--- src/core/init/repositories.ts
+++ src/core/init/repositories.ts
@@ -1,5 +1,6 @@
 import { db } from "@/src/core/config/Firebase";
 import { LeaderboardRepository } from "@/src/core/models/Leaderboard/repositories/LeaderboardRepository";
+import { OfferRepository } from "@/src/core/models/Offer/repositories/OfferRepository";
 
 export const LeadberboardRepo = LeaderboardRepository(db);
-
+export const OfferRepo = OfferRepository(db);
