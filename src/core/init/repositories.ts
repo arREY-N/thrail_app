ed19dd2e118c33b62e491b3ee60da9eb944f46ac
Reyn Penus <reypenus1224@gmail.com>
2026-08-05 23:02:06 +0800

    feat(cancellation): initialize CancellationRepo in repositories

--- src/core/init/repositories.ts
+++ src/core/init/repositories.ts
@@ -1,6 +1,8 @@
 import { db } from "@/src/core/config/Firebase";
+import { CancellationRepository } from "@/src/core/models/Cancellation/repositories/CancellationRepository";
 import { LeaderboardRepository } from "@/src/core/models/Leaderboard/repositories/LeaderboardRepository";
 import { OfferRepository } from "@/src/core/models/Offer/repositories/OfferRepository";
 
 export const LeadberboardRepo = LeaderboardRepository(db);
 export const OfferRepo = OfferRepository(db);
+export const CancellationRepo = CancellationRepository(db);
