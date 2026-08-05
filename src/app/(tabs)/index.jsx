09ea40a60c423275071f935758c1504b9bdfbfe4
Reyn Penus <reypenus1224@gmail.com>
2026-08-05 23:01:23 +0800

    feat(cancellation): implement cancellation request functionality in home component

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 import EmergencyNotification from '@/src/components/EmergencyNotification';
 import { useAppNavigation } from '@/src/core/hook/navigation/useAppNavigation';
 import useReview from '@/src/core/hook/review/useReview';
 import useTrailView from '@/src/core/hook/trail/useTrailView';
+import useCancellation from '@/src/core/models/Cancellation/hooks/useCancellation';
 import HomeScreen from '@/src/features/Home/screens/HomeScreen';
 
 export default function home(){
@@ -26,8 +27,30 @@ export default function home(){
         getItemRating,
     } = useReview();
 
+    const {
+        submitCancellationRequest,
+    } = useCancellation();
+
     return (
         <View style={{ flex: 1 }}>
+            <Pressable onPress={() => submitCancellationRequest({
+                reason: 'Test cancellation request',
+                offerId: 'bsI4zIzQkPjmgyZ8fumN',
+                businessId: 'smsV3pyBQpQMuRRcwUaQ7kLFYaZ2',
+                bookingId: '20eVkBPhSgRVdBLwgdlB',
+            })}>
+                <Text>Test Cancellation</Text>
+            </Pressable>
+            <View style={{ height: 20 }} />
+            <Pressable onPress={() => submitCancellationRequest({
+                reason: 'Test cancellation request',
+                businessId: 'test-business-id',
+                bookingId: 'test-booking-id',
+            })}>
+                <Text>Test Cancellation Error No Offer ID</Text>
+            </Pressable>
+            <View style={{ height: 20 }} />
+
             <HomeScreen 
                 locationTemp={{}} 
                 onWeatherPress={onWeatherPress}
