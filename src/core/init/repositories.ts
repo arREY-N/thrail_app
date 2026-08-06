b0a25dcb224cbeef53122f908c90ed34d924f94a
Reyn Penus <reypenus1224@gmail.com>
2026-08-06 18:03:32 +0800

    feat(repositories): add BookingRepository to initialization and fix export order

--- src/core/init/repositories.ts
+++ src/core/init/repositories.ts
@@ -1,4 +1,5 @@
 import { db } from "@/src/core/config/Firebase";
+import { BookingRepository } from "@/src/core/models/Booking/repositories/BookingRepository";
 import { CancellationRepository } from "@/src/core/models/Cancellation/repositories/CancellationRepository";
 import { LeaderboardRepository } from "@/src/core/models/Leaderboard/repositories/LeaderboardRepository";
 import { OfferRepository } from "@/src/core/models/Offer/repositories/OfferRepository";
@@ -6,3 +7,4 @@ import { OfferRepository } from "@/src/core/models/Offer/repositories/OfferRepos
 export const LeadberboardRepo = LeaderboardRepository(db);
 export const OfferRepo = OfferRepository(db);
 export const CancellationRepo = CancellationRepository(db);
+export const BookingRepo = BookingRepository(db);
