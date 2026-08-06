4f12841bc472e98dad4be1fdef2f320c42bc13a8
Reyn Penus <reypenus1224@gmail.com>
2026-08-06 18:06:40 +0800

    feat(cancellation): refactor submitCancellationRequest to use cancellationStore and include cancelledBy field

--- src/core/models/Cancellation/hooks/useCancellation.ts
+++ src/core/models/Cancellation/hooks/useCancellation.ts
@@ -1,7 +1,7 @@
 import { useAuthHook } from "@/src/core/hook/user/useAuthHook"
-import { CancellationRepo } from "@/src/core/init/repositories"
 import { createCancellationRequest } from "@/src/core/models/Cancellation/Cancellation"
 import { Cancellation } from "@/src/core/models/Cancellation/interfaces/ICancellation"
+import { useCancellationStore } from "@/src/core/models/Cancellation/stores/cancellationStore"
 import { useState } from "react"
 
 type CancellationRequest = Required<Pick<Cancellation, 'reason' | 'offerId' | 'businessId' | 'bookingId'>>
@@ -10,23 +10,33 @@ export default function useCancellation() {
     const { profile } = useAuthHook();
     const [localError, setLocalError] = useState<string | null>(null);
     const [request, setRequest] = useState<CancellationRequest | null>(null);
+    
+    const write = useCancellationStore(s => s.write);
 
     const submitCancellationRequest = async (request: CancellationRequest) => {
         try {
-    
+            
             if(!profile || !profile.id) {
                 throw new Error("User profile is not available.");
             }
+
+            if(profile.role === "admin") {
+                throw new Error("Only users can submit cancellation requests.");
+            }
     
             const cancellationRequest = createCancellationRequest(
-                { ...request, userId: profile.id }
+                { 
+                    ...request, 
+                    cancelledBy: "user", 
+                    userId: profile.id 
+                }
             ); 
 
             console.log("Cancellation Request Created:", cancellationRequest);
 
             setRequest(cancellationRequest);
 
-            await CancellationRepo.write(request.businessId, cancellationRequest)
+            await write(request.businessId, cancellationRequest)
         } catch (error) {
             console.error("Error submitting cancellation request:", (error as Error).message);
             setLocalError((error as Error).message || "An unexpected error occurred.");
