2f2d3ff3869f44303e1f2e9b73f876ee2c43cec9
Specter1031 <Ejcaraig1031@gmail.com>
2026-08-08 21:49:16 +0800

    modified: simplify viewOffer component by removing unused code and enhancing conditional rendering

--- src/app/(main)/admin/offer/view.tsx
+++ src/app/(main)/admin/offer/view.tsx
@@ -1,10 +1,7 @@
 import useOfferBooking from "@/src/core/hook/admin/useOfferBooking";
-import { Booking } from "@/src/core/models/Booking/Booking";
-import { Offer } from "@/src/core/models/Offer/Offer";
-import { formatDate } from "@/src/core/utility/date";
 import getSearchParam from "@/src/core/utility/getSearchParam";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { ActivityIndicator, Pressable, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
 import { useAppNavigation } from "@/src/core/hook/navigation/useAppNavigation";
 import OfferViewScreen from "@/src/features/Admin/screens/Offer/OfferViewScreen";
@@ -49,57 +46,16 @@ export default function viewOffer() {
         <>
             <Stack.Screen options={{ headerShown: false }} />
 
-            <OfferViewScreen
-                offerId={offerId}
-                offer={offer as any}
-                bookings={offerBookings}
-                onViewBooking={onViewBooking}
-                onBackPress={onBackPress}
-                error={error as any} 
-            />
+            {offer && (
+                <OfferViewScreen
+                    offerId={offerId}
+                    offer={offer}
+                    bookings={offerBookings}
+                    onViewBooking={onViewBooking}
+                    onBackPress={onBackPress}
+                    error={error || undefined} 
+                />
+            )}
         </>
-
-        // <TestOfferView
-        //     offerId={offerId}
-        //     offer={offer}
-        //     bookings={offerBookings}
-        //     onViewBooking={onViewBooking}
-        //     error={error} 
-        // />
-    )
-}
-
-export type ViewOfferParams = {
-    offerId: string;
-    offer: Offer  | null;
-    bookings: Booking[];
-    onViewBooking: (bookingId: string, offerId: string) => void;
-    error: string | null;
-}
-
-const TestOfferView = (params: ViewOfferParams) => {
-    if(!params.offer) return <Text>Offer not found</Text>;
-    
-    const offer = params.offer;
-
-    return(
-        <ScrollView>
-            { params.error && <Text>{params.error}</Text>}
-            <Text>{offer.name}</Text>
-            <Text>{offer.description}</Text>
-            <Text>{offer.price}</Text>
-
-            {params.bookings.length > 0 &&
-                params.bookings.map(b => {
-                    return (
-                        <Pressable onPress={() => params.onViewBooking(b.id, offer.id)} key={b.id} style={{ padding: 10, borderBottomWidth: 1, borderColor: '#ccc' }}>
-                            <Text>{b.user.username}</Text>
-                            <Text>{formatDate(b.createdAt)}</Text>
-                            <Text>{b.status}</Text>
-                        </Pressable>
-                    )
-                })
-            }
-        </ScrollView>
     )
 }
