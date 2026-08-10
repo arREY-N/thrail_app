fa5bf967e4b585a19addade2ed7765768967a99f
Reyn Penus <reypenus1224@gmail.com>
2026-08-10 20:08:20 +0800

    feat(cancellation): implement useCancellationAdminList hook to manage business cancellations and handle errors

--- src/core/models/Cancellation/hooks/useCancellationAdminList.ts
+++ src/core/models/Cancellation/hooks/useCancellationAdminList.ts
@@ -1,6 +1,39 @@
+import { useAuthHook } from "@/src/core/hook/user/useAuthHook";
+import { useCancellationStore } from "@/src/core/models/Cancellation/stores/cancellationStore";
+import { useEffect, useState } from "react";
+
 /**
  * Hook to fetch a list of cancellation items for admin users
  */
 export function useCancellationAdminList() {
+    const { businessId } = useAuthHook();
+    const [localError, setLocalError] = useState<string | null>(null);
+    const businessCancellations = useCancellationStore(s => s.businessCancellations);
+    const fetchAllBusinessCancellations = useCancellationStore(s => s.fetchAllBusinessCancellations);
+    const isFetching = useCancellationStore(s => s.isFetching);
+    
+    useEffect(() => {
+        if(!businessId) return;
 
+        fetchAllBusinessCancellations(businessId);
+    },[businessId]);
+
+    const refreshAdminCancellations = async () => {
+        try {
+            if(!businessId) 
+                throw new Error("Business ID is not available. Cannot refresh cancellations.");
+
+            fetchAllBusinessCancellations(businessId, true);
+        } catch (error) {
+            setLocalError(`Error refreshing cancellations: ${(error as Error).message}`);
+        }
+    }
+
+    
+    return {
+        refreshAdminCancellations,
+        businessCancellations,
+        isFetching,
+        localError
+    }
 }
