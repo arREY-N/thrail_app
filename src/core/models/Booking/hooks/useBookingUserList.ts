c72358654847e7bf4fd33ca97061bfdf57a79152
Reyn Penus <reypenus1224@gmail.com>
2026-08-10 20:06:12 +0800

    feat(booking): add custom hook for retrieving user bookings

---
+++ src/core/models/Booking/hooks/useBookingUserList.ts
@@ -0,0 +1,24 @@
+import { useAuthHook } from "@/src/core/hook/user/useAuthHook";
+import { useBookingsStore } from "@/src/core/models/Booking/stores/bookingStore";
+import { useEffect } from "react";
+
+export function useBookingUserList() {
+    const { profile } = useAuthHook();
+
+    const subscribeToUserBookings = useBookingsStore(s => s.subscribeToUserBookings); 
+    const bookings = useBookingsStore(s => s.userBookings);
+    const subscriptionError = useBookingsStore(s => s.subscriptionError);
+    
+    useEffect(() => {
+        if(!profile || !profile.id) return;
+
+        const unsubscribe = subscribeToUserBookings(profile.id);
+        
+        return () => unsubscribe();
+    }, [profile?.id]);
+
+    return {
+        bookings,
+        subscriptionError
+    };
+}
