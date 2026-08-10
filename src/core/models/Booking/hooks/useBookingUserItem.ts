e1e6d43a09d3a05fd35600d09fda6ed3bbafda9c
Reyn Penus <reypenus1224@gmail.com>
2026-08-10 20:05:55 +0800

    feat(booking): add custom hook for retrieving user booking by ID

---
+++ src/core/models/Booking/hooks/useBookingUserItem.ts
@@ -0,0 +1,9 @@
+import { useBookingsStore } from "@/src/core/models/Booking/stores/bookingStore";
+
+export function useBookingUserItem(bookingId: string) {
+    const booking = useBookingsStore(s => s.userBookings.find(b => b.id === bookingId) ?? null);
+
+    return {
+        booking
+    }
+}
