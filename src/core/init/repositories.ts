8137edb4ea317c2844e02239a8f40ae8fd42a6cb
Reyn Penus <reypenus1224@gmail.com>
2026-08-12 10:59:42 +0800

    modified(group): added group repository initialization

--- src/core/init/repositories.ts
+++ src/core/init/repositories.ts
@@ -1,6 +1,7 @@
 import { db } from "@/src/core/config/Firebase";
 import { BookingRepository } from "@/src/core/models/Booking/repositories/BookingRepository";
 import { CancellationRepository } from "@/src/core/models/Cancellation/repositories/CancellationRepository";
+import { GroupRepository } from "@/src/core/models/Group/repositories/GroupRepository";
 import { LeaderboardRepository } from "@/src/core/models/Leaderboard/repositories/LeaderboardRepository";
 import { OfferRepository } from "@/src/core/models/Offer/repositories/OfferRepository";
 
@@ -8,3 +9,4 @@ export const LeadberboardRepo = LeaderboardRepository(db);
 export const OfferRepo = OfferRepository(db);
 export const CancellationRepo = CancellationRepository(db);
 export const BookingRepo = BookingRepository(db);
+export const GroupRepo = GroupRepository(db);
