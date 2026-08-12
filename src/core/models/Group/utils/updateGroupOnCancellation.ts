594ea02cb2b7a7b5d7a3c9247759f15976b20de3
Reyn Penus <reypenus1224@gmail.com>
2026-08-12 14:01:51 +0800

    added(cancellation): created utility function to update group upon approved cancellation

---
+++ src/core/models/Group/utils/updateGroupOnCancellation.ts
@@ -0,0 +1,12 @@
+import { newGroup } from "@/src/core/models/Group/GroupFactory";
+import { Group } from "@/src/core/models/Group/interfaces/IGroup";
+
+export const updateGroupOnCancellation = (group: Group, removeId: string) => {
+    return newGroup({
+        ...group,
+        participantsIds: group.participantsIds.filter(id => id !== removeId),
+        members: group.members.filter(member => member.id !== removeId),
+        admins: group.admins.filter(admin => admin.id !== removeId),
+        updatedAt: new Date(),
+    })
+}
