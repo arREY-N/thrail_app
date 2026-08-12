4679c64d0259b46e5abb528f6f4f260c5ffecda7
Reyn Penus <reypenus1224@gmail.com>
2026-08-12 13:09:30 +0800

    added(utility): created function to handle errors caught in hooks but not in UI

---
+++ src/core/utility/errorFormatter.ts
@@ -0,0 +1,9 @@
+/**
+ * Formats error caught in the hook and logs it to prevent breaking the UI flow. 
+ * @param error - Error object containing the error message.
+ * @param errorState - A string that represents which hook state to access for the UI to display the error message. Default is 'error state'.
+ * @param hook - A string that represents the name of the hook where the error was caught. Default is 'Unknown Access'.
+ */
+export const catchError = (error: Error, errorState: string = 'error state', hook: string = 'Unknown Access'): void => {
+    console.error(`Error caught; access \`${errorState}\` via \`${hook}\` \n\n${error}`);
+}
