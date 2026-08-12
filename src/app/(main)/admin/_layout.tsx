0ac7ab937e27861aa2781559c7fe0b1ae2081c8c
Reyn Penus <reypenus1224@gmail.com>
2026-08-12 10:56:17 +0800

    refactor(layout): exchanged direct access to store with hook access

--- src/app/(main)/admin/_layout.tsx
+++ src/app/(main)/admin/_layout.tsx
@@ -1,13 +1,11 @@
 import LoadingScreen from "@/src/app/loading";
 import UnauthorizedScreen from "@/src/app/unauthorized";
-import { useAuthStore } from "@/src/core/stores/authStores/authStore";
+import { useAuthHook } from "@/src/core/hook/user/useAuthHook";
 import { Stack } from "expo-router";
 
 export default function adminLayout(){
-    const role = useAuthStore(s => s.role);
-    const loading = useAuthStore(s => s.isLoading);
-
-    if(loading) return <LoadingScreen/>
+    const { role, isLoading } = useAuthHook();
+    if(isLoading) return <LoadingScreen/>
 
     if(role !== 'admin') return <UnauthorizedScreen/>
 
