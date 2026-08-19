8127f4cb8c9a2bbe2af5b562be2d0cc9cbaf07cd
Reyn Penus <reypenus1224@gmail.com>
2026-08-20 07:39:57 +0800

    fix (issue36): added storage for bookings to fix feature blocking

--- src/core/models/Booking/stores/bookingStore.native.ts
+++ src/core/models/Booking/stores/bookingStore.native.ts
@@ -1,10 +1,15 @@
 import { BookingState, bookingStoreCreator } from "@/src/core/models/Booking/stores/bookingStoreCreator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const useBookingsStore = create<BookingState>()(
-    persist(immer(bookingStoreCreator), {
-        name: "booking-storage",
-    }),
-);
+    persist(
+        immer(bookingStoreCreator),
+        {
+            name: "booking-storage",
+            storage: createJSONStorage(() => AsyncStorage)
+        }
+    )
+)
