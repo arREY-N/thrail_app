558f0a48f6792fb4d4de5b52c83dcb433300776a
Reyn Penus <reypenus1224@gmail.com>
2026-09-22 16:49:40 +0800

    modified: moved update profile functions to UpdateUserFlow

---
+++ src/core/flows/UpdateUserFlow.ts
@@ -0,0 +1,40 @@
+import { IUser, newUser, useAuthHook, User, useUserStore } from "@/src/core/models/User/User";
+import { useState } from "react";
+
+export function UpdateUserFlow() {
+    const { profile, isLoading: authIsLoading } = useAuthHook();
+    const [isEditing, setIsEditing] = useState(false);
+    const isLoading = useUserStore(s => s.isLoading);
+
+    const createUser = useUserStore(s => s.create);
+
+    const onSaveAccount = async (updatedFields: Partial<IUser>) => {
+        const updatedUser: User = newUser({
+            ...profile,
+            ...updatedFields,
+        })
+
+        setIsEditing(false);
+        console.log('updated user: ', updatedUser);
+        await createUser(updatedUser);
+    }
+
+    const onEditPress = () => {
+        // TODO: [Backend] Implement navigation to edit profile screen
+        console.log("Edit Button clicked");
+        setIsEditing(true);
+    };
+
+    const onCancelPress = () => {
+        setIsEditing(false);
+    };
+
+    return {
+        onEditPress,
+        onCancelPress,
+        onSaveAccount,
+        isLoading: authIsLoading,
+        isEditing,
+        profile
+    }
+}
