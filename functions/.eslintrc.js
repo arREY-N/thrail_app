1fff92d426a1fa0571662aa8a80073cb1c038d8b
Zeed111 <veeennn.nn@gmail.com>
2026-09-23 05:24:36 +0800

    chore(functions): configure jest environment in eslint overrides for test files
    - Update overrides in functions/.eslintrc.js to match test files (**/*.test.*, **/__tests__/**)
    - Enable jest: true in test environment to recognize Jest globals (describe, it, expect, beforeEach, jest)

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["**/*.spec.*"],
+      files: ["**/*.spec.*", "**/*.test.*", "**/__tests__/**"],
       env: {
         mocha: true,
+        jest: true,
       },
       rules: {},
     },
